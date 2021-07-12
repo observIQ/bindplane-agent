6e69095fa20c4c629a1c44fc3c3041a33853b358
Brandon Johnson <brandon.johnson@bluemedora.com>
2021-07-12 14:17:27 -0400

    Add components (#2)
    * First pass adding components from contrib

--- cmd/observiqcol/components.go
+++ cmd/observiqcol/components.go
@@ -1,8 +1,19 @@
 package main
 
 import (
+	"github.com/open-telemetry/opentelemetry-collector-contrib/exporter/observiqexporter"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/extension/httpforwarder"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/extension/observer/ecsobserver"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/extension/observer/hostobserver"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/extension/observer/k8sobserver"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/extension/storage/filestorage"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/groupbyattrsprocessor"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/k8sprocessor"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/filelogreceiver"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/receivercreator"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/syslogreceiver"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/tcplogreceiver"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/udplogreceiver"
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/consumer/consumererror"
 	"go.opentelemetry.io/collector/service/defaultcomponents"
@@ -20,11 +31,11 @@ func components() (component.Factories, error) {
 	}
 
 	receivers := []component.ReceiverFactory{
-		// TODO: Figure out dependency issues with these
-		// filelogreceiver.NewFactory()
-		// syslogreceiver.NewFactory(),
-		// tcplogreceiver.NewFactory(),
-		// udplogreceiver.NewFactory(),
+		filelogreceiver.NewFactory(),
+		syslogreceiver.NewFactory(),
+		tcplogreceiver.NewFactory(),
+		udplogreceiver.NewFactory(),
+		receivercreator.NewFactory(),
 	}
 	for _, rf := range defaultFactories.Receivers {
 		receivers = append(receivers, rf)
@@ -32,19 +43,26 @@ func components() (component.Factories, error) {
 
 	processors := []component.ProcessorFactory{
 		groupbyattrsprocessor.NewFactory(),
-		// k8sprocessor.NewFactory(),
+		k8sprocessor.NewFactory(),
 	}
 	for _, pf := range defaultFactories.Processors {
 		processors = append(processors, pf)
 	}
 
-	exporters := []component.ExporterFactory{}
+	exporters := []component.ExporterFactory{
+		observiqexporter.NewFactory(), // TODO: This needs updating with a new release of the collector-contrib repo, exporter is out of date for now.
+	}
 	for _, ef := range defaultFactories.Exporters {
 		exporters = append(exporters, ef)
 	}
 
+	//TODO: oauth2clientauthextension -- having trouble with the imports for this one, but would be nice to have.
 	extensions := []component.ExtensionFactory{
 		filestorage.NewFactory(),
+		httpforwarder.NewFactory(),
+		ecsobserver.NewFactory(),
+		hostobserver.NewFactory(),
+		k8sobserver.NewFactory(),
 	}
 	for _, ef := range defaultFactories.Extensions {
 		extensions = append(extensions, ef)
--- go.mod
+++ go.mod
@@ -8,11 +8,30 @@ require (
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/golangci/golangci-lint v1.41.1
 	github.com/klauspost/compress v1.13.1 // indirect
+	github.com/open-telemetry/opentelemetry-collector-contrib/exporter/observiqexporter v0.29.0
+	github.com/open-telemetry/opentelemetry-collector-contrib/extension/httpforwarder v0.29.0
+	github.com/open-telemetry/opentelemetry-collector-contrib/extension/observer/ecsobserver v0.29.0
+	github.com/open-telemetry/opentelemetry-collector-contrib/extension/observer/hostobserver v0.29.0
+	github.com/open-telemetry/opentelemetry-collector-contrib/extension/observer/k8sobserver v0.29.0
 	github.com/open-telemetry/opentelemetry-collector-contrib/extension/storage v0.29.0
 	github.com/open-telemetry/opentelemetry-collector-contrib/processor/groupbyattrsprocessor v0.29.0
+	github.com/open-telemetry/opentelemetry-collector-contrib/processor/k8sprocessor v0.29.0
+	github.com/open-telemetry/opentelemetry-collector-contrib/receiver/filelogreceiver v0.29.0
+	github.com/open-telemetry/opentelemetry-collector-contrib/receiver/receivercreator v0.29.0
+	github.com/open-telemetry/opentelemetry-collector-contrib/receiver/syslogreceiver v0.29.0
+	github.com/open-telemetry/opentelemetry-collector-contrib/receiver/tcplogreceiver v0.29.0
+	github.com/open-telemetry/opentelemetry-collector-contrib/receiver/udplogreceiver v0.29.0
 	github.com/pierrec/lz4 v2.6.1+incompatible // indirect
 	github.com/stretchr/testify v1.7.0
 	go.opentelemetry.io/collector v0.29.0
 	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c
 	golang.org/x/tools v0.1.4
+)
+
+replace (
+	github.com/open-telemetry/opentelemetry-collector-contrib/extension/observer v0.0.0-00010101000000-000000000000 => github.com/open-telemetry/opentelemetry-collector-contrib/extension/observer v0.29.0
+	github.com/open-telemetry/opentelemetry-collector-contrib/extension/storage v0.0.0-00010101000000-000000000000 => github.com/open-telemetry/opentelemetry-collector-contrib/extension/storage v0.29.0
+	github.com/open-telemetry/opentelemetry-collector-contrib/internal/k8sconfig v0.0.0-00010101000000-000000000000 => github.com/open-telemetry/opentelemetry-collector-contrib/internal/k8sconfig v0.29.0
+	github.com/open-telemetry/opentelemetry-collector-contrib/internal/stanza v0.0.0-00010101000000-000000000000 => github.com/open-telemetry/opentelemetry-collector-contrib/internal/stanza v0.29.0
+	github.com/open-telemetry/opentelemetry-collector-contrib/internal/stanza v0.24.1-0.20210408210148-736647af91e1 => github.com/open-telemetry/opentelemetry-collector-contrib/internal/stanza v0.29.0
 )
