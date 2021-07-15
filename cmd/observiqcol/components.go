7118c6fbe5a33efd26652fb438fc8809a6a7f6c2
Brandon Johnson <brandon.johnson@bluemedora.com>
2021-07-15 11:09:19 -0400

    Be more choosy about components we are using, update contrib to 0.30.0

--- cmd/observiqcol/components.go
+++ cmd/observiqcol/components.go
@@ -3,90 +3,84 @@ package main
 import (
 	"github.com/open-telemetry/opentelemetry-collector-contrib/exporter/observiqexporter"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/extension/httpforwarder"
-	"github.com/open-telemetry/opentelemetry-collector-contrib/extension/observer/ecsobserver"
-	"github.com/open-telemetry/opentelemetry-collector-contrib/extension/observer/hostobserver"
-	"github.com/open-telemetry/opentelemetry-collector-contrib/extension/observer/k8sobserver"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/extension/storage/filestorage"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/groupbyattrsprocessor"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/k8sprocessor"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/filelogreceiver"
-	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/receivercreator"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/syslogreceiver"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/tcplogreceiver"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/udplogreceiver"
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/consumer/consumererror"
-	"go.opentelemetry.io/collector/service/defaultcomponents"
+	"go.opentelemetry.io/collector/exporter/fileexporter"
+	"go.opentelemetry.io/collector/exporter/loggingexporter"
+	"go.opentelemetry.io/collector/exporter/otlpexporter"
+	"go.opentelemetry.io/collector/exporter/otlphttpexporter"
+	"go.opentelemetry.io/collector/extension/bearertokenauthextension"
+	"go.opentelemetry.io/collector/extension/healthcheckextension"
+	"go.opentelemetry.io/collector/extension/oidcauthextension"
+	"go.opentelemetry.io/collector/extension/pprofextension"
+	"go.opentelemetry.io/collector/extension/zpagesextension"
+	"go.opentelemetry.io/collector/processor/attributesprocessor"
+	"go.opentelemetry.io/collector/processor/batchprocessor"
+	"go.opentelemetry.io/collector/processor/memorylimiter"
+	"go.opentelemetry.io/collector/processor/probabilisticsamplerprocessor"
+	"go.opentelemetry.io/collector/processor/resourceprocessor"
+	"go.opentelemetry.io/collector/receiver/otlpreceiver"
 )
 
 // Get the factories for components we want to use.
 // This includes all the defaults.
 func components() (component.Factories, error) {
-	defaultFactories, err := defaultcomponents.Components()
-
 	var errs []error
 
-	if err != nil {
-		errs = append(errs, err)
-	}
-
-	receivers := []component.ReceiverFactory{
+	receiverMap, err := component.MakeReceiverFactoryMap(
+		otlpreceiver.NewFactory(),
 		filelogreceiver.NewFactory(),
 		syslogreceiver.NewFactory(),
 		tcplogreceiver.NewFactory(),
 		udplogreceiver.NewFactory(),
-		receivercreator.NewFactory(),
-	}
-	for _, rf := range defaultFactories.Receivers {
-		receivers = append(receivers, rf)
+	)
+
+	if err != nil {
+		errs = append(errs, err)
 	}
 
-	processors := []component.ProcessorFactory{
+	processorMap, err := component.MakeProcessorFactoryMap(
 		groupbyattrsprocessor.NewFactory(),
 		k8sprocessor.NewFactory(),
-	}
-	for _, pf := range defaultFactories.Processors {
-		processors = append(processors, pf)
+		attributesprocessor.NewFactory(),
+		resourceprocessor.NewFactory(),
+		batchprocessor.NewFactory(),
+		memorylimiter.NewFactory(),
+		probabilisticsamplerprocessor.NewFactory(),
+	)
+
+	if err != nil {
+		errs = append(errs, err)
 	}
 
-	exporters := []component.ExporterFactory{
-		observiqexporter.NewFactory(), // TODO: This needs updating with a new release of the collector-contrib repo, exporter is out of date for now.
-	}
-	for _, ef := range defaultFactories.Exporters {
-		exporters = append(exporters, ef)
+	exporterMap, err := component.MakeExporterFactoryMap(
+		fileexporter.NewFactory(),
+		otlpexporter.NewFactory(),
+		otlphttpexporter.NewFactory(),
+		observiqexporter.NewFactory(),
+		loggingexporter.NewFactory(),
+	)
+
+	if err != nil {
+		errs = append(errs, err)
 	}
 
-	//TODO: oauth2clientauthextension -- having trouble with the imports for this one, but would be nice to have.
-	extensions := []component.ExtensionFactory{
+	extensionMap, err := component.MakeExtensionFactoryMap(
+		bearertokenauthextension.NewFactory(),
+		healthcheckextension.NewFactory(),
+		oidcauthextension.NewFactory(),
+		pprofextension.NewFactory(),
+		zpagesextension.NewFactory(),
 		filestorage.NewFactory(),
 		httpforwarder.NewFactory(),
-		ecsobserver.NewFactory(),
-		hostobserver.NewFactory(),
-		k8sobserver.NewFactory(),
-	}
-	for _, ef := range defaultFactories.Extensions {
-		extensions = append(extensions, ef)
-	}
-
-	receiverMap, err := component.MakeReceiverFactoryMap(receivers...)
-
-	if err != nil {
-		errs = append(errs, err)
-	}
-
-	processorMap, err := component.MakeProcessorFactoryMap(processors...)
-
-	if err != nil {
-		errs = append(errs, err)
-	}
-
-	exporterMap, err := component.MakeExporterFactoryMap(exporters...)
-
-	if err != nil {
-		errs = append(errs, err)
-	}
-
-	extensionMap, err := component.MakeExtensionFactoryMap(extensions...)
+	)
 
 	if err != nil {
 		errs = append(errs, err)
