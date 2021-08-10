ed7b259f0fc38b4695324af32e9c9137155c2197
Keith Schmitt <keith.schmitt@bluemedora.com>
2021-08-10 11:36:07 -0400

    fix merge issues

--- manager/status/status.go
+++ manager/status/status.go
@@ -1,6 +1,9 @@
 package status
 
 import (
+	"fmt"
+	"sync"
+
 	"github.com/observiq/observiq-collector/manager/message"
 )
 
@@ -13,11 +16,19 @@ const (
 	ERROR    Status = 2
 )
 
+type Metric struct {
+	Type      MetricKey   `json:"type"`
+	Timestamp int64       `json:"timestamp"`
+	Value     interface{} `json:"value"`
+}
+
 // Report is a status report.
 type Report struct {
-	ComponentType string `json:"componentType" mapstructure:"componentType"`
-	ComponentID   string `json:"componentID" mapstructure:"componentID"`
-	Status        Status `json:"status" mapstructure:"status"`
+	ComponentType string             `json:"componentType" mapstructure:"componentType"`
+	ComponentID   string             `json:"componentID" mapstructure:"componentID"`
+	Status        Status             `json:"status" mapstructure:"status"`
+	Metrics       map[string]*Metric `json:"metrics"`
+	sync.Mutex
 }
 
 // ToMessage converts a report into a message.
@@ -27,10 +38,34 @@ func (r *Report) ToMessage() *message.Message {
 }
 
 // Get returns the status of the collector.
-func Get() (Report, error) {
-	return Report{
+func Get() (*Report, error) {
+	report := Report{
 		ComponentType: "bpagent",
 		ComponentID:   "bpagent",
 		Status:        ACTIVE,
-	}, nil
+		Metrics:       make(map[string]*Metric),
+	}
+	err := report.addPerformanceMetrics()
+	if err != nil {
+		return nil, err
+	}
+	return &report, nil
+}
+
+type metricGatherer = func(sr *Report) error
+
+func (sr *Report) addPerformanceMetrics() error {
+	for _, metricGatherer := range []metricGatherer{AddCPUMetrics, AddMemoryMetrics, AddNetworkMetrics} {
+		err := metricGatherer(sr)
+		if err != nil {
+			return fmt.Errorf("there was an error gathering performance metrics. %s", err)
+		}
+	}
+	return nil
+}
+
+func (sr *Report) withMetric(m Metric) {
+	sr.Lock()
+	defer sr.Unlock()
+	sr.Metrics[string(m.Type)] = &m
 }
