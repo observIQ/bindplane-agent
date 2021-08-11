50c3e14b60395694fc2c0f74280e51204a652769
Joshua Williams <jmatthewwilliams89@gmail.com>
2021-08-11 14:49:47 -0400

    Added a restart task (#38)

---
+++ manager/task/restart.go
@@ -0,0 +1,27 @@
+package task
+
+import (
+	"errors"
+
+	"github.com/observiq/observiq-collector/collector"
+)
+
+// Restart is the task type assigned to restarting the collector.
+// Previously, this task was used to restart the entire manager.
+// For the sake of backwards compatability, the key remains the same.
+const Restart Type = "restartManager"
+
+// ExecuteRestart executes a restart task.
+func ExecuteRestart(task *Task, collector *collector.Collector) Response {
+	if task.Type != Restart {
+		err := errors.New("invalid type")
+		return task.Failure("task is not a restart", err)
+	}
+
+	err := collector.Restart()
+	if err != nil {
+		return task.Failure("failed to restart", err)
+	}
+
+	return task.Success()
+}
