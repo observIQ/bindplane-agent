1f0cd9578f97544c096b5c99074fdbe7e52a6cc0
Keith Schmitt <keith.schmitt@bluemedora.com>
2021-08-11 16:05:26 -0400

    fix not reporting errors yet

--- manager/status/status_test.go
+++ manager/status/status_test.go
@@ -1,7 +1,6 @@
 package status
 
 import (
-	"errors"
 	"testing"
 
 	"github.com/observiq/observiq-collector/collector"
@@ -21,12 +20,4 @@ func TestReportToMessage(t *testing.T) {
 
 	msg := report.ToMessage()
 	require.Equal(t, msg.Type, message.StatusReport)
-}
-
-func TestErrorToMessage(t *testing.T) {
-	status := collector.Status{Err: errors.New("Error for testing")}
-	report := Get("41794e4d-9564-4d98-9096-698302577c98", status)
-
-	msg := report.ToMessage()
-	require.Equal(t, msg.Content["status"], ERROR)
 }
