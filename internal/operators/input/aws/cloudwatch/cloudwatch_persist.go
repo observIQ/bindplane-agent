2731faf2ccff7159dbd76930615c1dfa79f78056
schmikei <keith.schmitt@bluemedora.com>
2021-09-08 16:18:40 -0400

    port over cloudwatch operator

---
+++ internal/operators/input/aws/cloudwatch/cloudwatch_persist.go
@@ -0,0 +1,34 @@
+package cloudwatch
+
+import (
+	"bytes"
+	"context"
+	"encoding/binary"
+	"fmt"
+
+	"github.com/open-telemetry/opentelemetry-log-collection/operator"
+)
+
+type Persister struct {
+	DB operator.Persister
+}
+
+// Helper function to get persisted data
+func (p *Persister) Read(ctx context.Context, key string) (int64, error) {
+	var startTimeBytes []byte
+	startTimeBytes, err := p.DB.Get(ctx, key)
+	if err != nil {
+		return -1, fmt.Errorf("there was an error reading from persistent storage: %w", err)
+	}
+	buffer := bytes.NewBuffer(startTimeBytes)
+	var startTime int64
+	err = binary.Read(buffer, binary.BigEndian, &startTime)
+	return startTime, err
+}
+
+// Helper function to set persisted data
+func (p *Persister) Write(ctx context.Context, key string, value int64) error {
+	var buf = make([]byte, 8)
+	binary.BigEndian.PutUint64(buf, uint64(value))
+	return p.DB.Set(ctx, key, buf)
+}
---
+++ internal/operators/input/aws/cloudwatch/cloudwatch_persist_test.go
@@ -0,0 +1,50 @@
+package cloudwatch
+
+import (
+	"context"
+	"testing"
+
+	"github.com/open-telemetry/opentelemetry-log-collection/operator"
+	"github.com/open-telemetry/opentelemetry-log-collection/testutil"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPersisterCache(t *testing.T) {
+	ctx := context.TODO()
+	stubDatabase := testutil.NewMockPersister("stub")
+	persister := Persister{
+		DB: operator.NewScopedPersister("test", stubDatabase),
+	}
+	err := persister.Write(ctx, "key", int64(1620666055012))
+	require.NoError(t, err)
+
+	value, readErr := persister.Read(ctx, "key")
+	require.NoError(t, readErr)
+	require.Equal(t, int64(1620666055012), value)
+}
+
+func TestPersisterLoad(t *testing.T) {
+	ctx := context.TODO()
+	persister := testutil.NewMockPersister("mock")
+	cwPersister := Persister{
+		DB: persister,
+	}
+	err := cwPersister.Write(ctx, "key", 1620666055012)
+	require.NoError(t, err)
+
+	value, syncErr := cwPersister.Read(ctx, "key")
+	require.NoError(t, syncErr)
+	require.Equal(t, int64(1620666055012), value)
+}
+
+func TestPersistentLoadNoKey(t *testing.T) {
+	ctx := context.TODO()
+
+	persister := testutil.NewMockPersister("mock")
+	cwPersister := Persister{
+		DB: persister,
+	}
+	value, err := cwPersister.Read(ctx, "key")
+	require.Error(t, err)
+	require.Equal(t, int64(0), value)
+}
