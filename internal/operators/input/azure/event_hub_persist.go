5c4b7ecad5ff095f4ec3776eb96d4b7f815d0154
Keith Schmitt <32067685+schmikei@users.noreply.github.com>
2021-09-09 17:15:25 -0400

    Port over azure eventhub related operators (#78)
    * port over azure eventhub
    
    * fix linting
    
    * register the operators
    
    * don't try to use channels this far deep, just use context.TODO
    
    * fix log analytics not setting persister

---
+++ internal/operators/input/azure/event_hub_persist.go
@@ -0,0 +1,47 @@
+package azure
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/Azure/azure-event-hubs-go/v3/persist"
+	"github.com/open-telemetry/opentelemetry-log-collection/operator"
+)
+
+// Persister implements persist.CheckpointPersister
+type Persister struct {
+	DB operator.Persister
+}
+
+// Write records an Azure Event Hub Checkpoint to the Stanza persistence backend
+func (p *Persister) Write(namespace, name, consumerGroup, partitionID string, checkpoint persist.Checkpoint) error {
+	key := p.persistenceKey(namespace, name, consumerGroup, partitionID)
+	value, err := json.Marshal(checkpoint)
+	if err != nil {
+		return err
+	}
+	return p.DB.Set(context.TODO(), key, value)
+}
+
+// Read retrieves an Azure Event Hub Checkpoint from the Stanza persistence backend
+func (p *Persister) Read(namespace, name, consumerGroup, partitionID string) (persist.Checkpoint, error) {
+	key := p.persistenceKey(namespace, name, consumerGroup, partitionID)
+	value, err := p.DB.Get(context.TODO(), key)
+	if err != nil {
+		return persist.Checkpoint{}, err
+	}
+
+	if len(value) < 1 {
+		return persist.Checkpoint{}, nil
+	}
+
+	var checkpoint persist.Checkpoint
+	err = json.Unmarshal(value, &checkpoint)
+	return checkpoint, err
+}
+
+func (p *Persister) persistenceKey(namespace, name, consumerGroup, partitionID string) string {
+	x := fmt.Sprintf("%s-%s-%s-%s", namespace, name, consumerGroup, partitionID)
+	return x
+}
