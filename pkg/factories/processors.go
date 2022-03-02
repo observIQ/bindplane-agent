6089e10f1f608254f739d72ff8e7cf4396e313d2
Corbin Phelps <corbin.phelps@bluemedora.com>
2022-03-02 10:33:06 -0500

    feat: Added processors to collector (#254)
    * Added processors to collector
    
    Signed-off-by: Corbin Phelps <corbin.phelps@bluemedora.com>
    
    * Removed redactionprocessor
    
    Signed-off-by: Corbin Phelps <corbin.phelps@bluemedora.com>

--- pkg/factories/processors.go
+++ pkg/factories/processors.go
@@ -18,12 +18,22 @@ import (
 	"github.com/GoogleCloudPlatform/opentelemetry-operations-collector/processor/normalizesumsprocessor"
 	"github.com/observiq/observiq-collector/pkg/processor/resourceattributetransposerprocessor"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/attributesprocessor"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/cumulativetodeltaprocessor"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/deltatorateprocessor"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/filterprocessor"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/groupbyattrsprocessor"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/groupbytraceprocessor"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/k8sattributesprocessor"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/metricsgenerationprocessor"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/metricstransformprocessor"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/probabilisticsamplerprocessor"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/resourcedetectionprocessor"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/resourceprocessor"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/routingprocessor"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/spanmetricsprocessor"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/spanprocessor"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/tailsamplingprocessor"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/transformprocessor"
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/component/componenttest"
 	"go.opentelemetry.io/collector/processor/batchprocessor"
@@ -34,13 +44,23 @@ var defaultProcessors = []component.ProcessorFactory{
 	attributesprocessor.NewFactory(),
 	batchprocessor.NewFactory(),
 	componenttest.NewNopProcessorFactory(),
+	cumulativetodeltaprocessor.NewFactory(),
+	deltatorateprocessor.NewFactory(),
+	filterprocessor.NewFactory(),
 	groupbyattrsprocessor.NewFactory(),
+	groupbytraceprocessor.NewFactory(),
 	k8sattributesprocessor.NewFactory(),
 	memorylimiterprocessor.NewFactory(),
+	metricsgenerationprocessor.NewFactory(),
 	metricstransformprocessor.NewFactory(),
 	normalizesumsprocessor.NewFactory(),
 	probabilisticsamplerprocessor.NewFactory(),
 	resourceattributetransposerprocessor.NewFactory(),
 	resourcedetectionprocessor.NewFactory(),
 	resourceprocessor.NewFactory(),
+	routingprocessor.NewFactory(),
+	spanmetricsprocessor.NewFactory(),
+	spanprocessor.NewFactory(),
+	tailsamplingprocessor.NewFactory(),
+	transformprocessor.NewFactory(),
 }
