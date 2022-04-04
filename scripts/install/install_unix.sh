ac37a3d0d768dbda06025901ce0efaaa128ed8ca
Corbin Phelps <corbin.phelps@bluemedora.com>
2022-04-04 09:34:21 -0400

    Removed shellcheck stuff at top of linux script
    Signed-off-by: Corbin Phelps <corbin.phelps@bluemedora.com>

--- scripts/install/install_unix.sh
+++ scripts/install/install_unix.sh
@@ -13,9 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# shellcheck disable=SC2119
-# SC2119 -> Use foo "$@" if function's $1 should mean script's $1.
-
 set -e
 
 # Collector Constants
