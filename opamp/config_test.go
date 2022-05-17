5711f3f26b8963669858a0ae74bb8be05f853957
Corbin Phelps <corbin.phelps@bluemedora.com>
2022-05-16 20:21:08 -0400

    Added more config tests
    Signed-off-by: Corbin Phelps <corbin.phelps@bluemedora.com>

--- opamp/config_test.go
+++ opamp/config_test.go
@@ -19,6 +19,7 @@ import (
 	"path/filepath"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -109,6 +110,170 @@ agent_id: 8321f735-a52c-4f49-aca9-66f9266c5fe5
 			}
 
 			require.Equal(t, tc.expectedConfig, cfg)
+		})
+	}
+}
+
+func TestCmpUpdatableFields(t *testing.T) {
+	secretKeyContents := "b92222ee-a1fc-4bb1-98db-26de3448541b"
+	nameOne, nameTwo := "one", "two"
+	labelsOne, labelsTwo := "one=1", "two=2"
+	testCase := []struct {
+		desc    string
+		baseCfg Config
+		compare Config
+		expect  bool
+	}{
+		{
+			desc: "Full match",
+			baseCfg: Config{
+				Endpoint:  "ws://localhost:1234",
+				SecretKey: &secretKeyContents,
+				AgentID:   "20ce90b8-506c-4a3b-8134-21aa8d526e03",
+				Labels:    &labelsOne,
+				AgentName: &nameOne,
+			},
+			compare: Config{
+				Endpoint:  "ws://localhost:1234",
+				SecretKey: &secretKeyContents,
+				AgentID:   "20ce90b8-506c-4a3b-8134-21aa8d526e03",
+				Labels:    &labelsOne,
+				AgentName: &nameOne,
+			},
+			expect: true,
+		},
+		{
+			desc: "Only Updatable fields match",
+			baseCfg: Config{
+				Endpoint:  "ws://localhost:1234",
+				SecretKey: &secretKeyContents,
+				AgentID:   "20ce90b8-506c-4a3b-8134-21aa8d526e03",
+				Labels:    &labelsOne,
+				AgentName: &nameOne,
+			},
+			compare: Config{
+				Endpoint:  "ws://some.host.com",
+				SecretKey: nil,
+				AgentID:   "d71cb88c-a4d3-4992-8bc8-d82702fdcb21",
+				Labels:    &labelsOne,
+				AgentName: &nameOne,
+			},
+			expect: true,
+		},
+		{
+			desc: "Labels match no Agent Name",
+			baseCfg: Config{
+				Endpoint:  "ws://localhost:1234",
+				SecretKey: &secretKeyContents,
+				AgentID:   "20ce90b8-506c-4a3b-8134-21aa8d526e03",
+				Labels:    &labelsOne,
+				AgentName: nil,
+			},
+			compare: Config{
+				Endpoint:  "ws://localhost:1234",
+				SecretKey: &secretKeyContents,
+				AgentID:   "20ce90b8-506c-4a3b-8134-21aa8d526e03",
+				Labels:    &labelsOne,
+				AgentName: nil,
+			},
+			expect: true,
+		},
+		{
+			desc: "Labels don't match no Agent Name",
+			baseCfg: Config{
+				Endpoint:  "ws://localhost:1234",
+				SecretKey: &secretKeyContents,
+				AgentID:   "20ce90b8-506c-4a3b-8134-21aa8d526e03",
+				Labels:    &labelsOne,
+				AgentName: nil,
+			},
+			compare: Config{
+				Endpoint:  "ws://localhost:1234",
+				SecretKey: &secretKeyContents,
+				AgentID:   "20ce90b8-506c-4a3b-8134-21aa8d526e03",
+				Labels:    &labelsTwo,
+				AgentName: nil,
+			},
+			expect: false,
+		},
+		{
+			desc: "Agent Name match no labels",
+			baseCfg: Config{
+				Endpoint:  "ws://localhost:1234",
+				SecretKey: &secretKeyContents,
+				AgentID:   "20ce90b8-506c-4a3b-8134-21aa8d526e03",
+				Labels:    nil,
+				AgentName: &nameOne,
+			},
+			compare: Config{
+				Endpoint:  "ws://localhost:1234",
+				SecretKey: &secretKeyContents,
+				AgentID:   "20ce90b8-506c-4a3b-8134-21aa8d526e03",
+				Labels:    nil,
+				AgentName: &nameOne,
+			},
+			expect: true,
+		},
+		{
+			desc: "Agent Name doesn't match no labels",
+			baseCfg: Config{
+				Endpoint:  "ws://localhost:1234",
+				SecretKey: &secretKeyContents,
+				AgentID:   "20ce90b8-506c-4a3b-8134-21aa8d526e03",
+				Labels:    nil,
+				AgentName: &nameOne,
+			},
+			compare: Config{
+				Endpoint:  "ws://localhost:1234",
+				SecretKey: &secretKeyContents,
+				AgentID:   "20ce90b8-506c-4a3b-8134-21aa8d526e03",
+				Labels:    nil,
+				AgentName: &nameTwo,
+			},
+			expect: false,
+		},
+		{
+			desc: "Label present in base not in other",
+			baseCfg: Config{
+				Endpoint:  "ws://localhost:1234",
+				SecretKey: &secretKeyContents,
+				AgentID:   "20ce90b8-506c-4a3b-8134-21aa8d526e03",
+				Labels:    &labelsOne,
+				AgentName: nil,
+			},
+			compare: Config{
+				Endpoint:  "ws://localhost:1234",
+				SecretKey: &secretKeyContents,
+				AgentID:   "20ce90b8-506c-4a3b-8134-21aa8d526e03",
+				Labels:    nil,
+				AgentName: nil,
+			},
+			expect: false,
+		},
+		{
+			desc: "Label present in other not in base",
+			baseCfg: Config{
+				Endpoint:  "ws://localhost:1234",
+				SecretKey: &secretKeyContents,
+				AgentID:   "20ce90b8-506c-4a3b-8134-21aa8d526e03",
+				Labels:    nil,
+				AgentName: nil,
+			},
+			compare: Config{
+				Endpoint:  "ws://localhost:1234",
+				SecretKey: &secretKeyContents,
+				AgentID:   "20ce90b8-506c-4a3b-8134-21aa8d526e03",
+				Labels:    &labelsTwo,
+				AgentName: nil,
+			},
+			expect: false,
+		},
+	}
+
+	for _, tc := range testCase {
+		t.Run(tc.desc, func(t *testing.T) {
+			actual := tc.baseCfg.CmpUpdatableFields(tc.compare)
+			assert.Equal(t, tc.expect, actual)
 		})
 	}
 }
