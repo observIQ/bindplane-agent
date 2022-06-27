89f2e9e95386f971536d9c46e5642074b296f67b
Brandon Johnson <brandon.johnson@bluemedora.com>
2022-06-27 13:13:41 -0400

    add version flag to updater

--- cmd/updater/main.go
+++ cmd/updater/main.go
@@ -1,4 +1,21 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/observiq/observiq-otel-collector/internal/version"
+	"github.com/spf13/pflag"
+)
+
 // Unimplemented
-func main() {}
+func main() {
+	var showVersion = pflag.BoolP("version", "v", false, "prints the version of the collector")
+	pflag.Parse()
+
+	if *showVersion {
+		fmt.Println("observiq-otel-collector updater version", version.Version())
+		fmt.Println("commit:", version.GitHash())
+		fmt.Println("built at:", version.Date())
+		return
+	}
+}
