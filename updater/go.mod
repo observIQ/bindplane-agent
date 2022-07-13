be6df511e21298c7222b73bf641c72f6ae6cb005
Brandon Johnson <brandon.johnson@bluemedora.com>
2022-07-13 13:17:18 -0400

    feat: Add ability to install unpacked artifacts in updater (#562)
    * start artifact install logic
    
    * fix uninstall service step
    
    * add tests for windows service manager
    
    * remove kardiano/service dependency
    
    * check filepath with spaces
    
    * more tests, hook up to main
    
    * naming
    
    * add licenses
    
    * gosec fixes
    
    * linux gosec + some lint issues
    
    * linter
    
    * fix formatting of windows service test
    
    * actually fix formatting
    
    * guard linux/win service tests behind tag
    
    * run tests as sudo on linux
    
    * fix inverted conditional
    
    * split updater integration tests into separate target
    
    * refactor package for better encapsulation
    
    * update darwin service to load/unload for start/stop
    
    * fix installDir for windows after rename
    
    * test replaceInstallDir
    
    * add license to service_test.go
    
    * fix make target phony
    
    * add some comments
    
    * add start of readme
    
    * add a (very basic) readme
    
    * use switch instead of multiple ifs
    
    * Add comments to moveFiles
    
    * fix failing darwin test

--- updater/go.mod
+++ updater/go.mod
@@ -3,9 +3,11 @@ module github.com/observiq/observiq-otel-collector/updater
 go 1.17
 
 require (
+	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51
 	github.com/mholt/archiver/v3 v3.5.1
 	github.com/spf13/pflag v1.0.5
 	github.com/stretchr/testify v1.7.2
+	golang.org/x/sys v0.0.0-20201015000850-e3ed0017c211
 )
 
 require (
@@ -18,6 +20,7 @@ require (
 	github.com/nwaples/rardecode v1.1.0 // indirect
 	github.com/pierrec/lz4/v4 v4.1.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.1.0 // indirect
 	github.com/ulikunitz/xz v0.5.9 // indirect
 	github.com/xi2/xz v0.0.0-20171230120015-48954b6210f8 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
---
+++ updater/internal/install/service_darwin.go
@@ -0,0 +1,113 @@
+// Copyright  observIQ, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//go:build darwin
+
+package install
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+)
+
+const (
+	darwinServiceFilePath = "/Library/LaunchDaemons/com.observiq.collector.plist"
+	darwinInstallDir      = "/opt/observiq-otel-collector"
+)
+
+// newService returns an instance of the Service interface for managing the observiq-otel-collector service on the current OS.
+func newService(latestPath string) Service {
+	return &darwinService{
+		newServiceFilePath:       filepath.Join(latestPath, "install", "com.observiq.collector.plist"),
+		installedServiceFilePath: darwinServiceFilePath,
+		installDir:               darwinInstallDir,
+	}
+}
+
+type darwinService struct {
+	// newServiceFilePath is the file path to the new plist file
+	newServiceFilePath string
+	// installedServiceFilePath is the file path to the installed plist file
+	installedServiceFilePath string
+	// installDir is the root directory of the main installation
+	installDir string
+}
+
+// Start the service
+func (d darwinService) Start() error {
+	// Launchctl exits with error code 0 if the file does not exist.
+	// We want to ensure that we error in this scenario.
+	if _, err := os.Stat(d.installedServiceFilePath); err != nil {
+		return fmt.Errorf("failed to stat installed service file: %w", err)
+	}
+
+	//#nosec G204 -- installedServiceFilePath is not determined by user input
+	cmd := exec.Command("launchctl", "load", d.installedServiceFilePath)
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("running launchctl failed: %w", err)
+	}
+	return nil
+}
+
+// Stop the service
+func (d darwinService) Stop() error {
+	// Launchctl exits with error code 0 if the file does not exist.
+	// We want to ensure that we error in this scenario.
+	if _, err := os.Stat(d.installedServiceFilePath); err != nil {
+		return fmt.Errorf("failed to stat installed service file: %w", err)
+	}
+
+	//#nosec G204 -- installedServiceFilePath is not determined by user input
+	cmd := exec.Command("launchctl", "unload", d.installedServiceFilePath)
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("running launchctl failed: %w", err)
+	}
+	return nil
+}
+
+// Installs the service
+func (d darwinService) Install() error {
+	serviceFileBytes, err := os.ReadFile(d.newServiceFilePath)
+	if err != nil {
+		return fmt.Errorf("failed to open input file: %w", err)
+	}
+
+	expandedServiceFileBytes := replaceInstallDir(serviceFileBytes, d.installDir)
+	if err := os.WriteFile(d.installedServiceFilePath, expandedServiceFileBytes, 0600); err != nil {
+		return fmt.Errorf("failed to write service file: %w", err)
+	}
+
+	return d.Start()
+}
+
+// Uninstalls the service
+func (d darwinService) Uninstall() error {
+	//#nosec G204 -- installedServiceFilePath is not determined by user input
+	if err := d.Stop(); err != nil {
+		return err
+	}
+
+	if err := os.Remove(d.installedServiceFilePath); err != nil {
+		return fmt.Errorf("failed to remove service file: %w", err)
+	}
+
+	return nil
+}
+
+// InstallDir returns the filepath to the install directory
+func installDir() (string, error) {
+	return darwinInstallDir, nil
+}
