5c7421b671c743b31eb027302b751b72b608fd06
Brandon Johnson <brandon.johnson@bluemedora.com>
2022-07-26 13:34:52 -0400

    remove rotation

--- updater/internal/logging/logging.go
+++ updater/internal/logging/logging.go
@@ -19,38 +19,20 @@ import (
 
 	"github.com/observiq/observiq-otel-collector/updater/internal/path"
 	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
-	"gopkg.in/natefinch/lumberjack.v2"
 )
 
 // NewLogger returns a new logger, that logs to the log directory relative to installDir,
 // with the provided log level.
 func NewLogger(installDir string) (*zap.Logger, error) {
-	prodConf := zap.NewProductionConfig()
+	conf := zap.NewProductionConfig()
+	conf.OutputPaths = []string{
+		path.LogFile(installDir),
+	}
 
-	prodLogger, err := prodConf.Build(zap.WrapCore(func(c zapcore.Core) zapcore.Core {
-		return core(installDir)
-	}))
-
+	prodLogger, err := conf.Build()
 	if err != nil {
-		return nil, fmt.Errorf("failed to make logger: %w", err)
+		return nil, fmt.Errorf("failed to build logger: %w", err)
 	}
 
 	return prodLogger, nil
-}
-
-func core(installDir string) zapcore.Core {
-	logger := &lumberjack.Logger{
-		Filename:   path.LogFile(installDir),
-		MaxSize:    10,
-		MaxBackups: 3,
-	}
-
-	return zapcore.NewCore(encoder(), zapcore.AddSync(logger), zapcore.DebugLevel)
-}
-
-func encoder() zapcore.Encoder {
-	encoderConfig := zap.NewProductionEncoderConfig()
-	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
-	return zapcore.NewJSONEncoder(encoderConfig)
 }
