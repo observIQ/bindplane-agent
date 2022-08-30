fa0d38713f5a43a25241540b6cd30a3d11a16639
Corbin Phelps <corbin.phelps@bluemedora.com>
2022-08-30 12:54:17 -0400

    Refactored snapshot buffers to use locks
    Signed-off-by: Corbin Phelps <corbin.phelps@bluemedora.com>

---
+++ internal/report/snapshot_buffers.go
@@ -0,0 +1,226 @@
+package report
+
+import (
+	"fmt"
+	"sync"
+
+	"go.opentelemetry.io/collector/pdata/plog"
+	"go.opentelemetry.io/collector/pdata/pmetric"
+	"go.opentelemetry.io/collector/pdata/ptrace"
+)
+
+type logBuffer struct {
+	mutex     sync.Mutex
+	buffer    []plog.Logs
+	idealSize int
+}
+
+func newLogBuffer(idealSize int) *logBuffer {
+	return &logBuffer{
+		buffer:    make([]plog.Logs, 0),
+		idealSize: idealSize,
+	}
+}
+
+func (l *logBuffer) Len() int {
+	l.mutex.Lock()
+	defer l.mutex.Unlock()
+
+	size := 0
+	for _, ld := range l.buffer {
+		size += ld.LogRecordCount()
+	}
+
+	return size
+}
+
+func (l *logBuffer) Add(ld plog.Logs) {
+	l.mutex.Lock()
+	defer l.mutex.Unlock()
+
+	logSize := ld.LogRecordCount()
+	bufferSize := l.Len()
+	switch {
+	// The number of logs is more than idealSize so reset this to just this log set
+	case logSize > l.idealSize:
+		l.buffer = []plog.Logs{ld}
+
+	// Haven't reached idealSize yet so add this
+	case logSize+bufferSize < l.idealSize:
+		l.buffer = append(l.buffer, ld)
+
+	// Adding this will put us over idealSize so and add the new logs.
+	// Only remove the oldest if it does not bring buffer under idealSize
+	case logSize+bufferSize >= l.idealSize:
+		l.buffer = append(l.buffer, ld)
+
+		// If removing the oldest item does not put us under the ideal size then it's ok to do so
+		oldest := l.buffer[0]
+		newBufferSize := logSize + bufferSize
+		if newBufferSize-oldest.LogRecordCount() >= l.idealSize {
+			l.buffer = l.buffer[1:]
+		}
+	}
+}
+
+func (l *logBuffer) ConstructPayload() ([]byte, error) {
+	l.mutex.Lock()
+	defer l.mutex.Unlock()
+	logsMarshaler := plog.NewProtoMarshaler()
+
+	payloadLogs := plog.NewLogs()
+	for _, ld := range l.buffer {
+		ld.ResourceLogs().CopyTo(payloadLogs.ResourceLogs())
+	}
+
+	payload, err := logsMarshaler.MarshalLogs(payloadLogs)
+	if err != nil {
+		return nil, fmt.Errorf("failed to construct payload: %w", err)
+	}
+
+	return payload, nil
+}
+
+type metricBuffer struct {
+	mutex     sync.Mutex
+	buffer    []pmetric.Metrics
+	idealSize int
+}
+
+func newMetricBuffer(idealSize int) *metricBuffer {
+	return &metricBuffer{
+		buffer:    make([]pmetric.Metrics, 0),
+		idealSize: idealSize,
+	}
+}
+
+func (l *metricBuffer) Len() int {
+	l.mutex.Lock()
+	defer l.mutex.Unlock()
+
+	size := 0
+	for _, md := range l.buffer {
+		size += md.DataPointCount()
+	}
+
+	return size
+}
+
+func (l *metricBuffer) Add(md pmetric.Metrics) {
+	l.mutex.Lock()
+	defer l.mutex.Unlock()
+
+	metricSize := md.DataPointCount()
+	bufferSize := l.Len()
+	switch {
+	// The number of metrics is more than idealSize so reset this to just this metric set
+	case metricSize > l.idealSize:
+		l.buffer = []pmetric.Metrics{md}
+
+	// Haven't reached idealSize yet so add this
+	case metricSize+bufferSize < l.idealSize:
+		l.buffer = append(l.buffer, md)
+
+	// Adding this will put us over idealSize so and add the new metrics.
+	// Only remove the oldest if it does not bring buffer under idealSize
+	case metricSize+bufferSize >= l.idealSize:
+		l.buffer = append(l.buffer, md)
+
+		// If removing the oldest item does not put us under the ideal size then it's ok to do so
+		oldest := l.buffer[0]
+		newBufferSize := metricSize + bufferSize
+		if newBufferSize-oldest.DataPointCount() >= l.idealSize {
+			l.buffer = l.buffer[1:]
+		}
+	}
+}
+
+func (l *metricBuffer) ConstructPayload() ([]byte, error) {
+	l.mutex.Lock()
+	defer l.mutex.Unlock()
+	metricMarshaler := pmetric.NewProtoMarshaler()
+
+	payloadMetrics := pmetric.NewMetrics()
+	for _, md := range l.buffer {
+		md.ResourceMetrics().CopyTo(payloadMetrics.ResourceMetrics())
+	}
+
+	payload, err := metricMarshaler.MarshalMetrics(payloadMetrics)
+	if err != nil {
+		return nil, fmt.Errorf("failed to construct payload: %w", err)
+	}
+
+	return payload, nil
+}
+
+type traceBuffer struct {
+	mutex     sync.Mutex
+	buffer    []ptrace.Traces
+	idealSize int
+}
+
+func newTraceBuffer(idealSize int) *traceBuffer {
+	return &traceBuffer{
+		buffer:    make([]ptrace.Traces, 0),
+		idealSize: idealSize,
+	}
+}
+
+func (l *traceBuffer) Len() int {
+	l.mutex.Lock()
+	defer l.mutex.Unlock()
+
+	size := 0
+	for _, td := range l.buffer {
+		size += td.SpanCount()
+	}
+
+	return size
+}
+
+func (l *traceBuffer) Add(td ptrace.Traces) {
+	l.mutex.Lock()
+	defer l.mutex.Unlock()
+
+	traceSize := td.SpanCount()
+	bufferSize := l.Len()
+	switch {
+	// The number of traces is more than idealSize so reset this to just this trace set
+	case traceSize > l.idealSize:
+		l.buffer = []ptrace.Traces{td}
+
+	// Haven't reached idealSize yet so add this
+	case traceSize+bufferSize < l.idealSize:
+		l.buffer = append(l.buffer, td)
+
+	// Adding this will put us over idealSize so and add the new traces.
+	// Only remove the oldest if it does not bring buffer under idealSize
+	case traceSize+bufferSize >= l.idealSize:
+		l.buffer = append(l.buffer, td)
+
+		// If removing the oldest item does not put us under the ideal size then it's ok to do so
+		oldest := l.buffer[0]
+		newBufferSize := traceSize + bufferSize
+		if newBufferSize-oldest.SpanCount() >= l.idealSize {
+			l.buffer = l.buffer[1:]
+		}
+	}
+}
+
+func (l *traceBuffer) ConstructPayload() ([]byte, error) {
+	l.mutex.Lock()
+	defer l.mutex.Unlock()
+	traceMarshaler := ptrace.NewProtoMarshaler()
+
+	payloadTraces := ptrace.NewTraces()
+	for _, md := range l.buffer {
+		md.ResourceSpans().CopyTo(payloadTraces.ResourceSpans())
+	}
+
+	payload, err := traceMarshaler.MarshalTraces(payloadTraces)
+	if err != nil {
+		return nil, fmt.Errorf("failed to construct payload: %w", err)
+	}
+
+	return payload, nil
+}
--- internal/report/snapshot_reporter.go
+++ internal/report/snapshot_reporter.go
@@ -59,9 +59,9 @@ type SnapshotReporter struct {
 	minPayloadSize int
 
 	// Buffers
-	logBuffers    map[string][]plog.Logs
-	metricBuffers map[string][]pmetric.Metrics
-	traceBuffers  map[string][]ptrace.Traces
+	logBuffers    map[string]*logBuffer
+	metricBuffers map[string]*metricBuffer
+	traceBuffers  map[string]*traceBuffer
 }
 
 // NewSnapshotReporter creates a new SnapshotReporter with the associated client
@@ -69,9 +69,9 @@ func NewSnapshotReporter(client Client) *SnapshotReporter {
 	return &SnapshotReporter{
 		client:         client,
 		minPayloadSize: 100,
-		logBuffers:     make(map[string][]plog.Logs),
-		metricBuffers:  make(map[string][]pmetric.Metrics),
-		traceBuffers:   make(map[string][]ptrace.Traces),
+		logBuffers:     make(map[string]*logBuffer),
+		metricBuffers:  make(map[string]*metricBuffer),
+		traceBuffers:   make(map[string]*traceBuffer),
 	}
 }
 
@@ -135,128 +135,61 @@ func (s *SnapshotReporter) Stop(context.Context) error {
 
 // SaveLogs saves off logs in a snapshot to be reported later
 func (s *SnapshotReporter) SaveLogs(componentID string, ld plog.Logs) {
-	componentLogs, ok := s.logBuffers[componentID]
+	buffer, ok := s.logBuffers[componentID]
 	if !ok {
-		componentLogs = make([]plog.Logs, 0)
+		buffer = newLogBuffer(s.minPayloadSize)
+		s.logBuffers[componentID] = buffer
 	}
 
-	currentPayloadSize := 0
-	for _, logs := range componentLogs {
-		currentPayloadSize += logs.LogRecordCount()
-	}
-
-	componentLogs = insertPayload(componentLogs, ld, ld.LogRecordCount(), currentPayloadSize, s.minPayloadSize)
-
-	s.logBuffers[componentID] = componentLogs
+	buffer.Add(ld)
 }
 
 // SaveTraces saves off traces in a snapshot to be reported later
 func (s *SnapshotReporter) SaveTraces(componentID string, td ptrace.Traces) {
-	componentTraces, ok := s.traceBuffers[componentID]
+	buffer, ok := s.traceBuffers[componentID]
 	if !ok {
-		componentTraces = make([]ptrace.Traces, 0)
+		buffer = newTraceBuffer(s.minPayloadSize)
+		s.traceBuffers[componentID] = buffer
 	}
 
-	currentPayloadSize := 0
-	for _, traces := range componentTraces {
-		currentPayloadSize += traces.SpanCount()
-	}
-
-	componentTraces = insertPayload(componentTraces, td, td.SpanCount(), currentPayloadSize, s.minPayloadSize)
-
-	s.traceBuffers[componentID] = componentTraces
+	buffer.Add(td)
 }
 
 // SaveMetrics saves off metrics in a snapshot to be reported later
 func (s *SnapshotReporter) SaveMetrics(componentID string, md pmetric.Metrics) {
-	componentMetrics, ok := s.metricBuffers[componentID]
+	buffer, ok := s.metricBuffers[componentID]
 	if !ok {
-		componentMetrics = make([]pmetric.Metrics, 0)
+		buffer = newMetricBuffer(s.minPayloadSize)
+		s.metricBuffers[componentID] = buffer
 	}
 
-	currentPayloadSize := 0
-	for _, metrics := range componentMetrics {
-		currentPayloadSize += metrics.DataPointCount()
-	}
-
-	componentMetrics = insertPayload(componentMetrics, md, md.DataPointCount(), currentPayloadSize, s.minPayloadSize)
-
-	s.metricBuffers[componentID] = componentMetrics
-}
-
-func insertPayload[T plog.Logs | pmetric.Metrics | ptrace.Traces](buffer []T, payload T, payloadSize, currentPayloadSize, minPayloadSize int) []T {
-	switch {
-	// The number of payload items is more than minPayloadSize so reset this to just this Log set
-	case payloadSize > minPayloadSize:
-		buffer = []T{payload}
-
-	// Haven't reached full size yet so add this
-	case payloadSize+currentPayloadSize < minPayloadSize:
-		buffer = append(buffer, payload)
-
-	// Adding this will put us over minimum so remove the oldest record and add the new one
-	case payloadSize+currentPayloadSize >= minPayloadSize:
-		buffer = append(buffer, payload)
-		buffer = buffer[1:]
-	}
-
-	return buffer
+	buffer.Add(md)
 }
 
 // prepRequestPayload based on the pipelineType will return a marshaled proto of the OTLP data types for the componentID
 func (s *SnapshotReporter) prepRequestPayload(componentID, pipelineType string) (payload []byte, err error) {
 	switch pipelineType {
 	case "logs":
-		logsMarshler := plog.NewProtoMarshaler()
-
-		payloadLogs := plog.NewLogs()
-		logs := s.logBuffers[componentID]
-		if len(logs) > 0 {
-			// Copy logs to a single payload
-			for _, ld := range logs {
-				ld.ResourceLogs().CopyTo(payloadLogs.ResourceLogs())
-			}
+		buffer, ok := s.logBuffers[componentID]
+		if !ok {
+			return []byte{}, nil
 		}
 
-		payload, err = logsMarshler.MarshalLogs(payloadLogs)
-		if err != nil {
-			return nil, fmt.Errorf("failed to construct payload: %w", err)
-		}
+		payload, err = buffer.ConstructPayload()
 	case "metrics":
-		metricsMarshler := pmetric.NewProtoMarshaler()
-
-		payloadMetrics := pmetric.NewMetrics()
-		metrics := s.metricBuffers[componentID]
-		if len(metrics) > 0 {
-			for _, md := range metrics {
-				md.ResourceMetrics().CopyTo(payloadMetrics.ResourceMetrics())
-			}
+		buffer, ok := s.metricBuffers[componentID]
+		if !ok {
+			return []byte{}, nil
 		}
 
-		payload, err = metricsMarshler.MarshalMetrics(payloadMetrics)
-		if err != nil {
-			return nil, fmt.Errorf("failed to construct payload: %w", err)
-		}
+		payload, err = buffer.ConstructPayload()
 	case "traces":
-		tracesMarshler := ptrace.NewProtoMarshaler()
-
-		payloadTraces := ptrace.NewTraces()
-		traces := s.traceBuffers[componentID]
-		if len(traces) > 0 {
-			for _, td := range traces {
-				td.ResourceSpans().CopyTo(payloadTraces.ResourceSpans())
-			}
+		buffer, ok := s.traceBuffers[componentID]
+		if !ok {
+			return []byte{}, nil
 		}
 
-		payload, err = tracesMarshler.MarshalTraces(payloadTraces)
-		if err != nil {
-			return nil, fmt.Errorf("failed to construct payload: %w", err)
-		}
-	}
-
-	// Case where there is no snapshot data for requested component and pipeline
-	if payload == nil {
-		payload = make([]byte, 0)
+		payload, err = buffer.ConstructPayload()
 	}
 
 	return
