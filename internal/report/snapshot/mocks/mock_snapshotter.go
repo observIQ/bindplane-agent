e2d7505955aebf6705f82872c1f0721adb9f3d48
Corbin Phelps <corbin.phelps@bluemedora.com>
2022-09-06 15:57:25 -0400

    Fixed linter error with mock package
    Signed-off-by: Corbin Phelps <corbin.phelps@bluemedora.com>

--- internal/report/snapshot/mocks/mock_snapshotter.go
+++ internal/report/snapshot/mocks/mock_snapshotter.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package mocks contains mocks for snapshot interfaces
 package mocks
 
 import (
