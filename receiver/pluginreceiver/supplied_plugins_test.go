298c7c32e9b2dbe3ac9716bb9d0a6be1b8f8c7b1
Corbin Phelps <corbin.phelps@bluemedora.com>
2022-10-11 10:25:03 -0400

    Added test to validate redenered config against otel components
    Signed-off-by: Corbin Phelps <corbin.phelps@bluemedora.com>

--- receiver/pluginreceiver/supplied_plugins_test.go
+++ receiver/pluginreceiver/supplied_plugins_test.go
@@ -15,13 +15,27 @@
 package pluginreceiver
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"path/filepath"
+	"runtime"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/extension/storage/filestorage"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/filterprocessor"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/metricstransformprocessor"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/transformprocessor"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/filelogreceiver"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/journaldreceiver"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/prometheusreceiver"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/sqlqueryreceiver"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/syslogreceiver"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/tcplogreceiver"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/udplogreceiver"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/windowseventlogreceiver"
 	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/config"
 )
 
@@ -33,6 +47,14 @@ func TestValidateSuppliedPlugins(t *testing.T) {
 	entries, err := os.ReadDir(pluginDirPath)
 	require.NoError(t, err)
 
+	// Create mock host and load up factories that are used in the current plugin set
+	host := &MockHost{}
+	// NOTE if adding a new plugin with new receiver, processors, or extensions ensure the factory is added to the below function
+	loadUsedPluginFactories(t, host)
+
+	// Used to get factories
+	emitterFactory := createLogEmitterFactory(nil)
+
 	tmp := t.TempDir()
 	t.Setenv("OIQ_OTEL_COLLECTOR_HOME", tmp)
 
@@ -41,17 +63,46 @@ func TestValidateSuppliedPlugins(t *testing.T) {
 		t.Run(fmt.Sprintf("Loading %s", entry.Name()), func(t *testing.T) {
 			t.Parallel()
 			fullFilePath, err := filepath.Abs(filepath.Join(pluginDirPath, entryName))
-			assert.NoError(t, err, "Failed to determine path of file %s", entryName)
+			require.NoError(t, err, "Failed to determine path of file %s", entryName)
 
 			// Load the plugin
 			plugin, err := LoadPlugin(fullFilePath)
-			assert.NoError(t, err, "Failed to load file %s", entryName)
+			require.NoError(t, err, "Failed to load file %s", entryName)
 
 			cfg, err := config.NewComponentIDFromString("test")
 			require.NoError(t, err)
 
-			_, err = plugin.Render(map[string]any{}, cfg)
-			assert.NoError(t, err, "Failed to render config for plugin %s", entryName)
+			// Render the config
+			renderedCfg, err := plugin.Render(map[string]any{}, cfg)
+			require.NoError(t, err, "Failed to render config for plugin %s", entryName)
+
+			// Check receivers and filter out checking those that are not supported by the OS
+			for id := range renderedCfg.Receivers {
+				componentID, err := config.NewComponentIDFromString(id)
+				require.NoError(t, err, "Failed to parse component ID %s for plugin %s", id, entryName)
+
+				switch componentID.Type() {
+				case windowseventlogreceiver.NewFactory().Type():
+					if runtime.GOOS != "windows" {
+						return
+					}
+				case journaldreceiver.NewFactory().Type():
+					if runtime.GOOS != "linux" {
+						return
+					}
+				}
+			}
+
+			// Setup to parse rendered config through actual receiver config logic
+			factories, err := renderedCfg.GetRequiredFactories(host, emitterFactory)
+			require.NoError(t, err, "Failed to get factories for plugin %s", entryName)
+
+			cfgProvider, err := renderedCfg.GetConfigProvider()
+			require.NoError(t, err, "Failed to get config provider for plugin %s", entryName)
+
+			_, err = cfgProvider.Get(context.Background(), *factories)
+			require.NoError(t, err, "Failed to validate config for plugin %s", entryName)
+
 		})
 	}
 }
@@ -81,4 +132,24 @@ func TestValidateSuppliedPluginsLoadSuppliedDefaults(t *testing.T) {
 			// We explicitly don't call checkRequired here, since parameters may not have specified defaults
 		})
 	}
+}
+
+func loadUsedPluginFactories(t *testing.T, host *MockHost) {
+	t.Helper()
+	// Receivers
+	host.On("GetFactory", component.KindReceiver, filelogreceiver.NewFactory().Type()).Return(filelogreceiver.NewFactory())
+	host.On("GetFactory", component.KindReceiver, tcplogreceiver.NewFactory().Type()).Return(tcplogreceiver.NewFactory())
+	host.On("GetFactory", component.KindReceiver, udplogreceiver.NewFactory().Type()).Return(udplogreceiver.NewFactory())
+	host.On("GetFactory", component.KindReceiver, syslogreceiver.NewFactory().Type()).Return(syslogreceiver.NewFactory())
+	host.On("GetFactory", component.KindReceiver, prometheusreceiver.NewFactory().Type()).Return(prometheusreceiver.NewFactory())
+	host.On("GetFactory", component.KindReceiver, sqlqueryreceiver.NewFactory().Type()).Return(sqlqueryreceiver.NewFactory())
+	host.On("GetFactory", component.KindReceiver, journaldreceiver.NewFactory().Type()).Return(journaldreceiver.NewFactory())
+
+	// Extensions
+	host.On("GetFactory", component.KindExtension, filestorage.NewFactory().Type()).Return(filestorage.NewFactory())
+
+	// Processors
+	host.On("GetFactory", component.KindProcessor, filterprocessor.NewFactory().Type()).Return(filterprocessor.NewFactory())
+	host.On("GetFactory", component.KindProcessor, metricstransformprocessor.NewFactory().Type()).Return(metricstransformprocessor.NewFactory())
+	host.On("GetFactory", component.KindProcessor, transformprocessor.NewFactory().Type()).Return(transformprocessor.NewFactory())
 }
