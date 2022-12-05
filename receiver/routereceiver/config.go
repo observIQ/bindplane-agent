5a9ae112aa5134495fdffda5a76dddaf6d66f0e6
jmwilliams89 <josh.williams@bluemedora.com>
2022-12-05 12:32:52 -0500

    Added package comment

--- receiver/routereceiver/config.go
+++ receiver/routereceiver/config.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package routereceiver provides a receiver that receives telemetry from other components.
 package routereceiver
 
 import (
