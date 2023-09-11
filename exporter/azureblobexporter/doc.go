16e6088caa11244d626e67ff65bc5d0a1a1b8113
Corbin Phelps <corbin.phelps@bluemedora.com>
2023-09-11 12:45:08 -0400

    Created stub for azure blob exporter
    Signed-off-by: Corbin Phelps <corbin.phelps@bluemedora.com>

---
+++ exporter/azureblobexporter/doc.go
@@ -0,0 +1,4 @@
+//go:generate mdatagen metadata.yaml
+
+// Package azureblobexporter stores OpenTelemetry data as an Azure Blob exporter.
+package azureblobexporter // import "github.com/observiq/bindplane-agent/exporter/azureblobexporter"
