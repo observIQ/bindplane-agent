820dcbaafb296c067a0b504fad520847fde34fdd
Brandon Johnson <brandon.johnson@observiq.com>
2024-02-01 11:57:37 -0500

    add tests for histograms, summaries, and exp-histograms

--- internal/report/snapshot/filter_metrics_test.go
+++ internal/report/snapshot/filter_metrics_test.go
@@ -41,57 +41,93 @@ func TestFilterMetrics(t *testing.T) {
 		minTimestamp    *time.Time
 		expectedFileOut string
 	}{
+		// {
+		// 	name:            "Matches attribute value (gauge)",
+		// 	fileIn:          filepath.Join(unfilteredMetricsDir, "host-metrics.yaml"),
+		// 	query:           asPtr("cool-attribute-value"),
+		// 	expectedFileOut: filepath.Join(filteredMetricsDir, "matches-attr-val-gauge.yaml"),
+		// },
+		// {
+		// 	name:            "Matches attribute key (gauge)",
+		// 	fileIn:          filepath.Join(unfilteredMetricsDir, "host-metrics.yaml"),
+		// 	query:           asPtr("cool-attribute-key"),
+		// 	expectedFileOut: filepath.Join(filteredMetricsDir, "matches-attr-key-gauge.yaml"),
+		// },
+		// {
+		// 	name:            "Matches attribute value (sum)",
+		// 	fileIn:          filepath.Join(unfilteredMetricsDir, "host-metrics.yaml"),
+		// 	query:           asPtr("extra-sum-attr-value"),
+		// 	expectedFileOut: filepath.Join(filteredMetricsDir, "matches-attr-val-sum.yaml"),
+		// },
+		// {
+		// 	name:            "Matches attribute key (sum)",
+		// 	fileIn:          filepath.Join(unfilteredMetricsDir, "host-metrics.yaml"),
+		// 	query:           asPtr("extra-sum-attr-key"),
+		// 	expectedFileOut: filepath.Join(filteredMetricsDir, "matches-attr-key-sum.yaml"),
+		// },
+		// {
+		// 	name:            "Matches resource attribute key",
+		// 	fileIn:          filepath.Join(unfilteredMetricsDir, "host-metrics.yaml"),
+		// 	query:           asPtr("extra-resource-attr-key"),
+		// 	expectedFileOut: filepath.Join(filteredMetricsDir, "matches-resource-attr-key.yaml"),
+		// },
+		// {
+		// 	name:            "Matches resource attribute value",
+		// 	fileIn:          filepath.Join(unfilteredMetricsDir, "host-metrics.yaml"),
+		// 	query:           asPtr("extra-resource-attr-value"),
+		// 	expectedFileOut: filepath.Join(filteredMetricsDir, "matches-resource-attr-val.yaml"),
+		// },
+		// {
+		// 	name:            "Matches metric name",
+		// 	fileIn:          filepath.Join(unfilteredMetricsDir, "host-metrics.yaml"),
+		// 	query:           asPtr("system.cpu.load_average.1m"),
+		// 	expectedFileOut: filepath.Join(filteredMetricsDir, "matches-metric-name.yaml"),
+		// },
+		// {
+		// 	name:   "Filters older than minimum timestamp",
+		// 	fileIn: filepath.Join(unfilteredMetricsDir, "host-metrics.yaml"),
+		// 	query:  asPtr("/dev"),
+		// 	// note when regenning goldens. golden.WriteMetrics
+		// 	// completely obliterates timestamps, so the regenerated golden will have the wrong timestamp.
+		// 	// You'll have to manually fix that.
+		// 	minTimestamp:    asPtr(time.Unix(0, 3000000)),
+		// 	expectedFileOut: filepath.Join(filteredMetricsDir, "filters-timestamp.yaml"),
+		// },
 		{
-			name:            "Matches attribute value (gauge)",
-			fileIn:          filepath.Join(unfilteredMetricsDir, "host-metrics.yaml"),
-			query:           asPtr("cool-attribute-value"),
-			expectedFileOut: filepath.Join(filteredMetricsDir, "matches-attr-val-gauge.yaml"),
+			name:            "Matches attribute value (histogram)",
+			fileIn:          filepath.Join(unfilteredMetricsDir, "histogram.yaml"),
+			query:           asPtr("dev-2"),
+			expectedFileOut: filepath.Join(filteredMetricsDir, "matches-attr-val-histogram.yaml"),
 		},
 		{
-			name:            "Matches attribute key (gauge)",
-			fileIn:          filepath.Join(unfilteredMetricsDir, "host-metrics.yaml"),
-			query:           asPtr("cool-attribute-key"),
-			expectedFileOut: filepath.Join(filteredMetricsDir, "matches-attr-key-gauge.yaml"),
+			name:            "Matches attribute key (histogram)",
+			fileIn:          filepath.Join(unfilteredMetricsDir, "histogram.yaml"),
+			query:           asPtr("prod-machine"),
+			expectedFileOut: filepath.Join(filteredMetricsDir, "matches-attr-key-histogram.yaml"),
 		},
 		{
-			name:            "Matches attribute value (sum)",
-			fileIn:          filepath.Join(unfilteredMetricsDir, "host-metrics.yaml"),
-			query:           asPtr("extra-sum-attr-value"),
-			expectedFileOut: filepath.Join(filteredMetricsDir, "matches-attr-val-sum.yaml"),
+			name:            "Matches attribute value (exponential histogram)",
+			fileIn:          filepath.Join(unfilteredMetricsDir, "exp-histogram.yaml"),
+			query:           asPtr("dev-2"),
+			expectedFileOut: filepath.Join(filteredMetricsDir, "matches-attr-val-exp-histogram.yaml"),
 		},
 		{
-			name:            "Matches attribute key (sum)",
-			fileIn:          filepath.Join(unfilteredMetricsDir, "host-metrics.yaml"),
-			query:           asPtr("extra-sum-attr-key"),
-			expectedFileOut: filepath.Join(filteredMetricsDir, "matches-attr-key-sum.yaml"),
+			name:            "Matches attribute key (exponential histogram)",
+			fileIn:          filepath.Join(unfilteredMetricsDir, "exp-histogram.yaml"),
+			query:           asPtr("prod-machine"),
+			expectedFileOut: filepath.Join(filteredMetricsDir, "matches-attr-key-exp-histogram.yaml"),
 		},
 		{
-			name:            "Matches resource attribute key",
-			fileIn:          filepath.Join(unfilteredMetricsDir, "host-metrics.yaml"),
-			query:           asPtr("extra-resource-attr-key"),
-			expectedFileOut: filepath.Join(filteredMetricsDir, "matches-resource-attr-key.yaml"),
+			name:            "Matches attribute value (summary)",
+			fileIn:          filepath.Join(unfilteredMetricsDir, "summary.yaml"),
+			query:           asPtr("dev-2"),
+			expectedFileOut: filepath.Join(filteredMetricsDir, "matches-attr-val-summary.yaml"),
 		},
 		{
-			name:            "Matches resource attribute value",
-			fileIn:          filepath.Join(unfilteredMetricsDir, "host-metrics.yaml"),
-			query:           asPtr("extra-resource-attr-value"),
-			expectedFileOut: filepath.Join(filteredMetricsDir, "matches-resource-attr-val.yaml"),
-		},
-		{
-			name:            "Matches metric name",
-			fileIn:          filepath.Join(unfilteredMetricsDir, "host-metrics.yaml"),
-			query:           asPtr("system.cpu.load_average.1m"),
-			expectedFileOut: filepath.Join(filteredMetricsDir, "matches-metric-name.yaml"),
-		},
-		{
-			name:   "Filters older than minimum timestamp",
-			fileIn: filepath.Join(unfilteredMetricsDir, "host-metrics.yaml"),
-			query:  asPtr("/dev"),
-			// note when regenning goldens. golden.WriteMetrics
-			// completely obliterates timestamps, so the regenerated golden will have the wrong timestamp.
-			// You'll have to manually fix that.
-			minTimestamp:    asPtr(time.Unix(0, 3000000)),
-			expectedFileOut: filepath.Join(filteredMetricsDir, "filters-timestamp.yaml"),
+			name:            "Matches attribute key (summary)",
+			fileIn:          filepath.Join(unfilteredMetricsDir, "summary.yaml"),
+			query:           asPtr("prod-machine"),
+			expectedFileOut: filepath.Join(filteredMetricsDir, "matches-attr-key-summary.yaml"),
 		},
 	}
 
@@ -102,8 +138,8 @@ func TestFilterMetrics(t *testing.T) {
 
 			metricsOut := filterMetrics(metricsIn, tc.query, tc.minTimestamp)
 
-			// err = golden.WriteMetrics(t, tc.expectedFileOut, metricsOut)
-			// require.NoError(t, err)
+			err = golden.WriteMetrics(t, tc.expectedFileOut, metricsOut)
+			require.NoError(t, err)
 
 			expectedMetricsOut, err := golden.ReadMetrics(tc.expectedFileOut)
 			require.NoError(t, err)
