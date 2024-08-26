12c9adf816e7ca973928ee2c40286a54ab41a848
Dakota Paasman <122491662+dpaasman00@users.noreply.github.com>
2024-08-26 08:23:03 -0400

    feat: Windows installation and packaging (#1775)
    * wip
    
    * initial testing
    
    * add goreleaser logging
    
    * install tools
    
    * simplify manual msi build action
    
    * fix supervisor binary
    
    * polish
    
    * change back service name
    
    * remove goreleaser logs
    
    * update supervisor labels field
    
    * add supervisor_storage, rename cfg to supervisor.yaml, try removing supervisor storage on uninstall
    
    * cleanup logging and use -r flag when removing supervisor_storage
    
    * include supervisor_storage dir

---
+++ windows/install/generate-supervisor-yaml.bat
@@ -0,0 +1,71 @@
+@echo off
+setlocal
+
+set "install_dir=%~1"
+set "endpoint=%~2"
+set "secret_key=%~3"
+set "labels=%~4"
+
+echo %install_dir%
+echo %endpoint%
+echo %secret_key%
+echo %labels%
+
+if "%endpoint%"=="" (
+    echo Endpoint not specified; Not writing output yaml
+    exit /b 0
+)
+
+if "%secret_key%"=="" (
+    echo Secret Key not specified; Not writing output yaml
+    exit /b 0
+)
+
+set "supervisorFile=%install_dir%supervisor.yaml"
+set "agentBinary=%install_dir%observiq-otel-collector.exe"
+
+echo Writing manager yaml
+
+set "serverField=server:"
+set "endpointField=  endpoint: "%endpoint%""
+set "headersField=  headers:"
+set "authorizationField=    Authorization: "Secret-Key %secret_key%""
+set "tlsField=  tls:"
+set "insecureField=    insecure: true"
+set "insecureSkipField=    insecure_skip_verify: true"
+
+set "capabilitiesField=capabilities:"
+set "acceptsRemoteCfgField=  accepts_remote_config: true"
+set "reportsRemoteCfgField=  reports_remote_config: true"
+
+set "agentField=agent:"
+set "executablePathField=  executable: '%agentBinary%'"
+set "descriptionField=  description:"
+set "nonIdentifyingAttributesField=    non_identifying_attributes:"
+set "serviceLabelsField=      service.labels: "%labels%""
+
+set "storageField=storage:"
+set "directoryField=  directory: '%install_dir%supervisor_storage'"
+
+echo %serverField% >"%supervisorFile%"
+echo %endpointField% >>"%supervisorFile%"
+echo %headersField% >>"%supervisorFile%"
+echo %authorizationField% >>"%supervisorFile%"
+echo %tlsField% >>"%supervisorFile%"
+echo %insecureField% >>"%supervisorFile%"
+echo %insecureSkipField% >>"%supervisorFile%"
+
+echo %capabilitiesField% >>"%supervisorFile%"
+echo %acceptsRemoteCfgField% >>"%supervisorFile%"
+echo %reportsRemoteCfgField% >>"%supervisorFile%"
+
+echo %agentField% >>"%supervisorFile%"
+echo %executablePathField% >>"%supervisorFile%"
+echo %descriptionField% >>"%supervisorFile%"
+echo %nonIdentifyingAttributesField% >>"%supervisorFile%"
+echo %serviceLabelsField% >>"%supervisorFile%"
+
+echo %storageField% >>"%supervisorFile%"
+echo %directoryField% >>"%supervisorFile%"
+
+endlocal
--- windows/test/uninstall.rb
+++
@@ -1,5 +0,0 @@
-describe service('observiq-otel-collector') do
-    it { should_not be_enabled }
-    it { should_not be_installed }
-    it { should_not be_running }
-end
