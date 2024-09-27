cebc6b8a5f0795da0c11e1576a5b9d5141a1182b
Brandon Johnson <brandon.johnson@observiq.com>
2024-09-27 08:58:26 -0400

    make the update-otel script easier to update

--- scripts/update-otel.sh
+++ scripts/update-otel.sh
@@ -13,6 +13,34 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# This is the list of stable (v1.0.0+) modules in the core repository.
+read -d '' STABLE_MODULES << EOF
+go.opentelemetry.io/collector/pdata
+go.opentelemetry.io/collector/config/configtls
+go.opentelemetry.io/collector/config/configretry
+go.opentelemetry.io/collector/confmap
+go.opentelemetry.io/collector/config/configopaque
+go.opentelemetry.io/collector/confmap/provider/envprovider
+go.opentelemetry.io/collector/confmap/provider/fileprovider
+go.opentelemetry.io/collector/config/confignet
+EOF
+
+is_stable_module() {
+    for stable_mod in $STABLE_MODULES; do
+        if [ "$stable_mod" = "$1" ]; then
+            return 0
+        fi
+    done
+    return 1
+}
+
+is_contrib_module() {
+    case $1 in
+    github.com/open-telemetry/opentelemetry-collector-contrib*)
+        return 0;;
+    esac
+    return 1
+}
 
 TARGET_VERSION=$1
 if [ -z "$TARGET_VERSION" ]; then
@@ -40,25 +68,23 @@ do
     (
         echo "Updating deps in $local_mod"
         cd "$local_mod" || exit 1
+        # go list will not work if module is not tidy, so we tidy first
+        go mod tidy -compat=1.22
+
         OTEL_MODULES=$(go list -m -f '{{if not (or .Indirect .Main)}}{{.Path}}{{end}}' all |
             grep -E -e '(?:^github.com/open-telemetry/opentelemetry-collector-contrib)|(?:^go.opentelemetry.io/collector)')
 
         for mod in $OTEL_MODULES; do
-            case $mod in
-            go.opentelemetry.io/collector/pdata* | go.opentelemetry.io/collector/config/configtls* | go.opentelemetry.io/collector/config/configretry* | go.opentelemetry.io/collector/confmap | go.opentelemetry.io/collector/config/configopaque | go.opentelemetry.io/collector/confmap/provider/envprovider | go.opentelemetry.io/collector/confmap/provider/fileprovider | go.opentelemetry.io/collector/config/confignet)
-                # pdata package is versioned separately
+            if is_stable_module "$mod"; then
                 echo "$local_mod: $mod@$PDATA_TARGET_VERSION"
                 go mod edit -require "$mod@$PDATA_TARGET_VERSION"
-                ;; 
-            github.com/open-telemetry/opentelemetry-collector-contrib*)
+            elif is_contrib_module "$mode"; then
                 echo "$local_mod: $mod@$CONTRIB_TARGET_VERSION"
                 go mod edit -require "$mod@$CONTRIB_TARGET_VERSION"
-                ;;
-            *)
+            else
                 echo "$local_mod: $mod@$TARGET_VERSION"
                 go mod edit -require "$mod@$TARGET_VERSION"
-                ;;
-            esac;
+            fi
         done
     )
 done
