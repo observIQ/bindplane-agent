46510f83716b05fe4bd78bf10e82886b3a049ba7
Dakota Paasman <122491662+dpaasman00@users.noreply.github.com>
2024-10-04 09:10:00 -0400

    wip 2

--- updater/internal/install/install.go
+++ updater/internal/install/install.go
@@ -22,11 +22,13 @@ import (
 	"os"
 	"path/filepath"
 
+	"github.com/google/uuid"
 	"github.com/observiq/bindplane-agent/updater/internal/action"
 	"github.com/observiq/bindplane-agent/updater/internal/file"
 	"github.com/observiq/bindplane-agent/updater/internal/path"
 	"github.com/observiq/bindplane-agent/updater/internal/rollback"
 	"github.com/observiq/bindplane-agent/updater/internal/service"
+	"github.com/oklog/ulid/v2"
 	"go.uber.org/zap"
 	"gopkg.in/yaml.v3"
 )
@@ -36,7 +38,7 @@ import (
 //go:generate mockery --name Installer --filename mock_installer.go --structname MockInstaller
 type Installer interface {
 	// Install installs new artifacts over the old ones.
-	Install(rollback.Rollbacker) error
+	Install(rollback.Rollbacker, int) error
 }
 
 // archiveInstaller allows you to install files from latestDir into installDir,
@@ -63,7 +65,7 @@ func NewInstaller(logger *zap.Logger, installDir string, service service.Service
 // Install installs the unpacked artifacts in latestDir to installDir,
 // as well as installing the new service file using the installer's Service interface.
 // It then starts the service.
-func (i archiveInstaller) Install(rb rollback.Rollbacker) error {
+func (i archiveInstaller) Install(rb rollback.Rollbacker, hcePort int) error {
 	// If JMX jar exists outside of install directory, make sure that gets backed up
 	if err := i.attemptSpecialJMXJarInstall(rb); err != nil {
 		return fmt.Errorf("failed to process special JMX jar: %w", err)
@@ -77,7 +79,7 @@ func (i archiveInstaller) Install(rb rollback.Rollbacker) error {
 	i.logger.Debug("Install artifacts copied")
 
 	// translate manager.yaml into supervisor config
-	if err := translateManagerToSupervisor(i.logger, i.installDir, i.backupDir, rb); err != nil {
+	if err := initializeSupervisorConfig(i.logger, i.installDir, i.backupDir, rb, hcePort); err != nil {
 		return fmt.Errorf("failed to translate manager config into supervisor config: %w", err)
 	}
 	i.logger.Debug("Translated config files")
@@ -233,6 +235,7 @@ func skipConfigFiles(path string) bool {
 
 type Headers struct {
 	Authorization string `yaml:"Authorization"`
+	AgentIDFormat string `yaml:"X-Bindplane-Agent-Id-Format"`
 }
 
 type TLS struct {
@@ -252,7 +255,8 @@ type Capabilities struct {
 }
 
 type Agent struct {
-	Executable string `yaml:"executable"`
+	Executable      string `yaml:"executable"`
+	HealthCheckPort int    `yaml:"health_check_port"`
 }
 
 type Storage struct {
@@ -266,34 +270,55 @@ type SupervisorConfig struct {
 	Storage      Storage      `yaml:"storage"`
 }
 
-func translateManagerToSupervisor(logger *zap.Logger, installDir, backupDir string, rb rollback.Rollbacker) error {
-	// Read in endpoint and secret-key values from manager.yaml
+func initializeSupervisorConfig(logger *zap.Logger, installDir, backupDir string, rb rollback.Rollbacker, hcePort int) error {
+	// Open manager.yaml
 	managerPath := filepath.Join(installDir, "manager.yaml")
 	data, err := os.ReadFile(managerPath)
 	if err != nil {
-		return fmt.Errorf("failed to read manager.yaml: %w", err)
+		return fmt.Errorf("read manager.yaml: %w", err)
 	}
 	var manager map[string]any
 	err = yaml.Unmarshal(data, &manager)
 	if err != nil {
-		return fmt.Errorf("failed to unmarshal manager yaml: %w", err)
+		return fmt.Errorf("unmarshal manager yaml: %w", err)
 	}
-	var endpoint, secretKey string
+
+	supervisorCfg, err := createSupervisorConfig(logger, manager, installDir, hcePort)
+	if err != nil {
+		return fmt.Errorf("create supervisor config: %w", err)
+	}
+
+	err = writeSupervisorConfig(logger, supervisorCfg, installDir, managerPath, rb)
+	if err != nil {
+		return fmt.Errorf("write supervisor config")
+	}
+
+	err = handleAgentIDConversion(logger, manager, installDir, rb)
+	if err != nil {
+		return fmt.Errorf("convert agent id: %w", err)
+	}
+
+	return nil
+}
+
+func createSupervisorConfig(logger *zap.Logger, manager map[string]any, installDir string, hcePort int) (*SupervisorConfig, error) {
+	// Read manager values
 	var ok bool
+	var endpoint, secretKey string
 	if endpoint, ok = manager["endpoint"].(string); !ok {
-		return fmt.Errorf("failed to read in endpoint: %w", err)
+		return nil, errors.New("read in endpoint")
 	}
-	if secretKey, ok = manager["secretKey"].(string); !ok {
-		return fmt.Errorf("failed to read in secret key: %w", err)
+	if secretKey, ok = manager["secret_key"].(string); !ok {
+		return nil, errors.New("read in secret key")
 	}
-	logger.Debug("successfully read in manager config values")
 
 	// Construct new supervisor config
-	supervisorCfg := SupervisorConfig{
+	return &SupervisorConfig{
 		Server: Server{
 			Endpoint: endpoint,
 			Headers: Headers{
 				Authorization: "Secret-Key " + secretKey,
+				AgentIDFormat: "ULID",
 			},
 			TLS: TLS{
 				Insecure:           true,
@@ -305,54 +330,108 @@ func translateManagerToSupervisor(logger *zap.Logger, installDir, backupDir stri
 			ReportsRemoteConfig: true,
 		},
 		Agent: Agent{
-			Executable: filepath.Join(installDir, "observiq-otel-collector"),
+			Executable:      filepath.Join(installDir, "observiq-otel-collector"),
+			HealthCheckPort: hcePort,
 		},
 		Storage: Storage{
-			// TODO: need to make this dir?
-			Directory: filepath.Join(installDir, "storage"),
+			Directory: filepath.Join(installDir, "supervisor_storage"),
 		},
-	}
+	}, nil
+}
+
+func writeSupervisorConfig(logger *zap.Logger, supervisorCfg *SupervisorConfig, installDir, managerPath string, rb rollback.Rollbacker) error {
 	supervisorYaml, err := yaml.Marshal(supervisorCfg)
 	if err != nil {
-		return fmt.Errorf("failed to marshal supervisor yaml: %w", err)
+		return fmt.Errorf("marshal supervisor yaml: %w", err)
 	}
 
-	// Create supervisor.yaml and write to it
 	supervisorPath := filepath.Join(installDir, "supervisor.yaml")
 
 	// We create the action record here, because we want to record the file does not exist
 	// before we open the file (which will end up creating the file).
-	// Use CopyFileAction because rollback can be used to delete supervisor.yaml and restore manager.yaml
-	// without creating a new rollback action.
-	cfa, err := action.NewCopyFileAction(logger, managerPath, supervisorPath, backupDir)
+	// Use CopyFileAction because rollback can be used to delete supervisor.yaml.
+	// Leave fromPathRel & backupDir empty bc supervisor is a new file.
+	cfa, err := action.NewCopyFileAction(logger, "", supervisorPath, "")
 	if err != nil {
-		return fmt.Errorf("failed to create copy file action: %w", err)
+		return fmt.Errorf("create supervisor.yaml copy file action: %w", err)
 	}
-	// Record that we are performing copying the file.
-	// We record before we actually do the action here because the file may be partially written,
-	// and we will want to roll that back if that is the case.
 	rb.AppendAction(cfa)
 
 	supervisorFile, err := os.OpenFile(filepath.Clean(supervisorPath), os.O_CREATE|os.O_WRONLY, fs.FileMode(0600))
 	if err != nil {
-		return fmt.Errorf("failed to open supervisor config file: %w", err)
+		return fmt.Errorf("open supervisor config file: %w", err)
 	}
 	defer func() {
 		err := supervisorFile.Close()
 		if err != nil {
-			logger.Error("failed to close supervisor config file", zap.Error(err))
+			logger.Error("close supervisor config file", zap.Error(err))
 		}
 	}()
 
 	_, err = supervisorFile.Write(supervisorYaml)
 	if err != nil {
-		return fmt.Errorf("failed to write supervisor config: %w", err)
+		return fmt.Errorf("write supervisor config: %w", err)
+	}
+	return nil
+}
+
+type PersistentState struct {
+	InstanceID string `yaml:"instance_id"`
+}
+
+func handleAgentIDConversion(logger *zap.Logger, manager map[string]any, installDir string, rb rollback.Rollbacker) error {
+	// Retrieve agent id and convert to UUID
+	agentID, ok := manager["agent_id"].(string)
+	if !ok {
+		return fmt.Errorf("read in agent id")
+	}
+	agentULID, err := ulid.Parse(agentID)
+	if err != nil {
+		return fmt.Errorf("parse agent id into a ULID: %w", err)
+	}
+	ulidBytes, err := agentULID.MarshalBinary()
+	if err != nil {
+		return fmt.Errorf("marshal ulid to bytes: %w", err)
+	}
+	agentUUID, err := uuid.FromBytes(ulidBytes)
+	if err != nil {
+		return fmt.Errorf("convert agent id to UUID: %w", err)
+	}
+	persistentStateYaml, err := yaml.Marshal(&PersistentState{InstanceID: agentUUID.String()})
+	if err != nil {
+		return fmt.Errorf("marshal persistent_state yaml: %w", err)
 	}
 
-	// Delete manager.yaml
-	if err = os.Remove(managerPath); err != nil {
-		return fmt.Errorf("failed to delete manager config file: %w", err)
+	// Create supervisor_storage dir and persistent_state.yaml
+	storageDirPath := filepath.Join(installDir, "supervisor_storage")
+	persistentStatePath := filepath.Join(storageDirPath, "persistent_state.yaml")
+
+	// Create rollback action for creating 'supervisor_storage' dir.
+	// If update fails, rollbacker will delete this dir && persistent_state.yaml file inside it.
+	cfa, err := action.NewCopyFileAction(logger, "", storageDirPath, "")
+	if err != nil {
+		return fmt.Errorf("create supervisor_storage copy file action")
+	}
+	rb.AppendAction(cfa)
+
+	if err := os.Mkdir(storageDirPath, 0750); err != nil {
+		return fmt.Errorf("create supervisor_storage dir: %w", err)
 	}
 
+	persistentStateFile, err := os.OpenFile(filepath.Clean(persistentStatePath), os.O_CREATE|os.O_WRONLY, fs.FileMode(0600))
+	if err != nil {
+		return fmt.Errorf("open persistent_state.yaml: %w", err)
+	}
+	defer func() {
+		err := persistentStateFile.Close()
+		if err != nil {
+			logger.Error("close persistent_state.yaml file", zap.Error(err))
+		}
+	}()
+
+	_, err = persistentStateFile.Write(persistentStateYaml)
+	if err != nil {
+		return fmt.Errorf("write persistent_state.yaml: %w", err)
+	}
 	return nil
 }
