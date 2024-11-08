6079244504abb42fae4b1c367bce4c3b4e22e5a1
Caleb Hurshman <caleb@hurshman.com>
2024-11-08 11:07:16 -0500

    lint

--- receiver/splunksearchapireceiver/api.go
+++ receiver/splunksearchapireceiver/api.go
@@ -1,3 +1,18 @@
+// Copyright observIQ, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package splunksearchapireceiver provides a receiver that uses the Splunk API to migrate event data.
 package splunksearchapireceiver
 
 import (
@@ -10,8 +25,8 @@ import (
 	"net/http"
 )
 
-func createHttpClient() *http.Client {
-	// TODO: Add functionality to configure TLS settings using config options
+func createHTTPClient() *http.Client {
+	// TODO: Add TLS
 	tr := &http.Transport{
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true}, // Disables TLS verification
 	}
@@ -29,7 +44,7 @@ func createSearchJob(config *Config, search string) (CreateJobResponse, error) {
 	}
 	req.SetBasicAuth(config.Username, config.Password)
 
-	client := createHttpClient()
+	client := createHTTPClient()
 	resp, err := client.Do(req)
 	if err != nil {
 		return CreateJobResponse{}, err
@@ -63,7 +78,7 @@ func getJobStatus(config *Config, sid string) (JobStatusResponse, error) {
 	}
 	req.SetBasicAuth(config.Username, config.Password)
 
-	client := createHttpClient()
+	client := createHTTPClient()
 	resp, err := client.Do(req)
 	if err != nil {
 		return JobStatusResponse{}, err
@@ -95,7 +110,7 @@ func getSearchResults(config *Config, sid string) (SearchResults, error) {
 	}
 	req.SetBasicAuth(config.Username, config.Password)
 
-	client := createHttpClient()
+	client := createHTTPClient()
 	resp, err := client.Do(req)
 	if err != nil {
 		return SearchResults{}, err
