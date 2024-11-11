8ec2d768a2ef5005fc3adac41451ce169110d319
Caleb Hurshman <caleb@hurshman.com>
2024-11-11 11:10:19 -0500

    package comment

--- receiver/splunksearchapireceiver/client.go
+++ receiver/splunksearchapireceiver/client.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package splunksearchapireceiver contains the Splunk Search API receiver.
 package splunksearchapireceiver
 
 import (
