53bc633f5dedea141c8fd95904aefd19316fc1d1
Caleb Hurshman <caleb@hurshman.com>
2024-11-13 13:56:21 -0500

    rm unnecessary clauses

--- receiver/splunksearchapireceiver/config_test.go
+++ receiver/splunksearchapireceiver/config_test.go
@@ -16,7 +16,6 @@ package splunksearchapireceiver
 
 import (
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/require"
 )
@@ -154,66 +153,6 @@ func TestValidate(t *testing.T) {
 			errText:     "latest_time failed to parse as RFC3339",
 		},
 		{
-			desc:     "earliest_time after latest_time",
-			endpoint: "http://localhost:8089",
-			username: "user",
-			password: "password",
-			searches: []Search{
-				{
-					Query:        "search index=_internal",
-					EarliestTime: "2024-10-30T14:00:00.000Z",
-					LatestTime:   "2024-10-30T04:00:00.000Z",
-				},
-			},
-			errExpected: true,
-			errText:     "earliest_time must be earlier than latest_time",
-		},
-		{
-			desc:     "earliest_time and latest_time equal",
-			endpoint: "http://localhost:8089",
-			username: "user",
-			password: "password",
-			searches: []Search{
-				{
-					Query:        "search index=_internal",
-					EarliestTime: "2024-10-30T14:00:00.000Z",
-					LatestTime:   "2024-10-30T14:00:00.000Z",
-				},
-			},
-			errExpected: true,
-			errText:     "earliest_time must be earlier than latest_time",
-		},
-		{
-			desc:     "earliest_time in the future",
-			endpoint: "http://localhost:8089",
-			username: "user",
-			password: "password",
-			searches: []Search{
-				{
-					Query:        "search index=_internal",
-					EarliestTime: time.Now().Add(1 * time.Hour).Format(time.RFC3339),
-					LatestTime:   time.Now().Add(10 * time.Hour).Format(time.RFC3339),
-				},
-			},
-			errExpected: true,
-			errText:     "earliest_time must be earlier than current time",
-		},
-		{
-			desc:     "latest_time in the future",
-			endpoint: "http://localhost:8089",
-			username: "user",
-			password: "password",
-			searches: []Search{
-				{
-					Query:        "search index=_internal",
-					EarliestTime: "2024-10-30T04:00:00.000Z",
-					LatestTime:   time.Now().Add(10 * time.Hour).Format(time.RFC3339),
-				},
-			},
-			errExpected: true,
-			errText:     "latest_time must be earlier than or equal to current time",
-		},
-		{
 			desc:     "Invalid query chaining",
 			endpoint: "http://localhost:8089",
 			username: "user",
@@ -285,12 +224,6 @@ func TestValidate(t *testing.T) {
 			cfg.Password = tc.password
 			cfg.Searches = tc.searches
 			err := cfg.Validate()
-			if tc.errExpected && err == nil {
-				t.Errorf("expected error, got nil")
-			}
-			if !tc.errExpected && err != nil {
-				t.Errorf("unexpected error: %v", err)
-			}
 			if tc.errExpected {
 				require.EqualError(t, err, tc.errText)
 				return
