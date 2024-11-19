21b21e1346d0a36c032b58e1140465aa0d3fb0a5
Caleb Hurshman <caleb@hurshman.com>
2024-11-19 15:12:02 -0500

    client tests

--- receiver/splunksearchapireceiver/client_test.go
+++ receiver/splunksearchapireceiver/client_test.go
@@ -15,8 +15,10 @@
 package splunksearchapireceiver
 
 import (
+	"io"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -31,10 +33,20 @@ var (
 )
 
 func TestCreateSearchJob(t *testing.T) {
-	// valid search
 	resp, err := testClient.CreateSearchJob("index=otel")
 	require.NoError(t, err)
 	require.Equal(t, "123456", resp.SID)
+
+	// returns an error if the response status isn't 201
+	resp, err = testClient.CreateSearchJob("index=fail_to_create_job")
+	require.ErrorContains(t, err, "failed to create search job")
+	require.Empty(t, resp)
+
+	// returns an error if the response body can't be unmarshalled
+	resp, err = testClient.CreateSearchJob("index=fail_to_unmarshal")
+	require.ErrorContains(t, err, "failed to unmarshal search job create response")
+	require.Empty(t, resp)
+
 }
 
 func TestGetJobStatus(t *testing.T) {
@@ -42,24 +54,58 @@ func TestGetJobStatus(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, "DONE", resp.Content.Dict.Keys[0].Value)
 	require.Equal(t, "text/xml", resp.Content.Type)
+
+	// returns an error if the response status isn't 200
+	resp, err = testClient.GetJobStatus("654321")
+	require.ErrorContains(t, err, "failed to get search job status")
+	require.Empty(t, resp)
+
+	// returns an error if the response body can't be unmarshalled
+	resp, err = testClient.GetJobStatus("098765")
+	require.ErrorContains(t, err, "failed to unmarshal search job status response")
+	require.Empty(t, resp)
 }
 
 func TestGetSearchResults(t *testing.T) {
+	resp, err := testClient.GetSearchResults("123456", 0, 5)
+	require.NoError(t, err)
+	require.Equal(t, 5, len(resp.Results))
+	require.Equal(t, "Hello, world!", resp.Results[0].Raw)
 
+	// returns an error if the response status isn't 200
+	resp, err = testClient.GetSearchResults("654321", 0, 5)
+	require.ErrorContains(t, err, "failed to get search job results")
+	require.Empty(t, resp)
+
+	// returns an error if the response body can't be unmarshalled
+	resp, err = testClient.GetSearchResults("098765", 0, 5)
+	require.ErrorContains(t, err, "failed to unmarshal search job results response")
+	require.Empty(t, resp)
 }
 
 // mock Splunk servers
 func newMockSplunkServer() *httptest.Server {
 	return httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		switch req.URL.Path {
+		switch req.URL.String() {
 		case "/services/search/jobs":
-			rw.Header().Set("Content-Type", "application/xml")
-			rw.WriteHeader(http.StatusCreated)
-			rw.Write([]byte(`<?xml version="1.0" encoding="UTF-8"?>
-			<response>
-				<sid>123456</sid>
-			</response>
-			`))
+			body, _ := io.ReadAll(req.Body)
+			if strings.Contains(string(body), "index=otel") {
+				rw.Header().Set("Content-Type", "application/xml")
+				rw.WriteHeader(http.StatusCreated)
+				rw.Write([]byte(`<?xml version="1.0" encoding="UTF-8"?>
+				<response>
+					<sid>123456</sid>
+				</response>
+				`))
+			}
+			if strings.Contains(string(body), "index=fail_to_create_job") {
+				rw.WriteHeader(http.StatusNotFound)
+			}
+			if strings.Contains(string(body), "index=fail_to_unmarshal") {
+				rw.WriteHeader(http.StatusCreated)
+				rw.Write([]byte(`invalid xml`))
+				req.Body = &errorReader{}
+			}
 		case "/services/search/v2/jobs/123456":
 			rw.Header().Set("Content-Type", "application/xml")
 			rw.WriteHeader(http.StatusOK)
@@ -71,10 +117,20 @@ func newMockSplunkServer() *httptest.Server {
 					</dict>
 				</content>
 			</response>`))
-		case "/services/search/v2/jobs/123456/results":
+		case "/services/search/v2/jobs/654321":
+			rw.WriteHeader(http.StatusNotFound)
+		case "/services/search/v2/jobs/098765":
+			rw.WriteHeader(http.StatusOK)
+			rw.Write([]byte(`invalid xml`))
+		case "/services/search/v2/jobs/123456/results?output_mode=json&offset=0&count=5":
 			rw.Header().Set("Content-Type", "application/json")
 			rw.WriteHeader(http.StatusOK)
 			rw.Write(splunkEventsResultsP1)
+		case "/services/search/v2/jobs/654321/results?output_mode=json&offset=0&count=5":
+			rw.WriteHeader(http.StatusNotFound)
+		case "/services/search/v2/jobs/098765/results?output_mode=json&offset=0&count=5":
+			rw.WriteHeader(http.StatusOK)
+			rw.Write([]byte(`invalid json`))
 		default:
 			rw.WriteHeader(http.StatusNotFound)
 		}
@@ -106,3 +162,13 @@ var splunkEventsResultsP1 = []byte(`{
 		}
 	]
 }`)
+
+type errorReader struct{}
+
+func (e *errorReader) Read(p []byte) (n int, err error) {
+	return 0, io.ErrUnexpectedEOF
+}
+
+func (e *errorReader) Close() error {
+	return nil
+}
