a44a3f965a63aa4c53ba1be72bb61c5785984c27
Dan Jaglowski <jaglows3@gmail.com>
2024-12-04 22:00:01 -0500

    wip

--- exporter/chronicleexporter/exporter_test.go
+++ exporter/chronicleexporter/exporter_test.go
@@ -18,13 +18,18 @@ import (
 	"context"
 	"errors"
 	"testing"
+	"time"
 
 	"github.com/golang/mock/gomock"
 	"github.com/observiq/bindplane-agent/exporter/chronicleexporter/protos/api"
 	"github.com/observiq/bindplane-agent/exporter/chronicleexporter/protos/api/mocks"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/collector/component/componenttest"
 	"go.opentelemetry.io/collector/consumer/consumererror"
+	"go.opentelemetry.io/collector/exporter/exportertest"
+	"go.opentelemetry.io/collector/pdata/pcommon"
+	"go.opentelemetry.io/collector/pdata/plog"
 	"go.uber.org/zap"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -50,11 +55,11 @@ func TestLogsDataPusher(t *testing.T) {
 				marshaller := NewMockMarshaler(t)
 				marshaller.On("MarshalRawLogs", mock.Anything, mock.Anything).Return([]*api.BatchCreateLogsRequest{{}}, nil)
 				return &chronicleExporter{
-					cfg:       &cfg,
-					metrics:   newExporterMetrics([]byte{}, []byte{}, "", cfg.Namespace),
-					logger:    zap.NewNop(),
-					client:    mockClient,
-					marshaler: marshaller,
+					cfg:        &cfg,
+					metrics:    newExporterMetrics([]byte{}, []byte{}, "", cfg.Namespace),
+					logger:     zap.NewNop(),
+					grpcClient: mockClient,
+					marshaler:  marshaller,
 				}
 			},
 			setupMocks: func(mockClient *mocks.MockIngestionServiceV2Client) {
@@ -69,11 +74,11 @@ func TestLogsDataPusher(t *testing.T) {
 				marshaller := NewMockMarshaler(t)
 				marshaller.On("MarshalRawLogs", mock.Anything, mock.Anything).Return([]*api.BatchCreateLogsRequest{{}}, nil)
 				return &chronicleExporter{
-					cfg:       &cfg,
-					metrics:   newExporterMetrics([]byte{}, []byte{}, "", cfg.Namespace),
-					logger:    zap.NewNop(),
-					client:    mockClient,
-					marshaler: marshaller,
+					cfg:        &cfg,
+					metrics:    newExporterMetrics([]byte{}, []byte{}, "", cfg.Namespace),
+					logger:     zap.NewNop(),
+					grpcClient: mockClient,
+					marshaler:  marshaller,
 				}
 			},
 			setupMocks: func(mockClient *mocks.MockIngestionServiceV2Client) {
@@ -90,11 +95,11 @@ func TestLogsDataPusher(t *testing.T) {
 				marshaller := NewMockMarshaler(t)
 				marshaller.On("MarshalRawLogs", mock.Anything, mock.Anything).Return([]*api.BatchCreateLogsRequest{{}}, nil)
 				return &chronicleExporter{
-					cfg:       &cfg,
-					metrics:   newExporterMetrics([]byte{}, []byte{}, "", cfg.Namespace),
-					logger:    zap.NewNop(),
-					client:    mockClient,
-					marshaler: marshaller,
+					cfg:        &cfg,
+					metrics:    newExporterMetrics([]byte{}, []byte{}, "", cfg.Namespace),
+					logger:     zap.NewNop(),
+					grpcClient: mockClient,
+					marshaler:  marshaller,
 				}
 			},
 			setupMocks: func(mockClient *mocks.MockIngestionServiceV2Client) {
@@ -112,11 +117,11 @@ func TestLogsDataPusher(t *testing.T) {
 				// Simulate an error during log marshaling
 				marshaller.On("MarshalRawLogs", mock.Anything, mock.Anything).Return(nil, errors.New("marshal error"))
 				return &chronicleExporter{
-					cfg:       &cfg,
-					metrics:   newExporterMetrics([]byte{}, []byte{}, "", cfg.Namespace),
-					logger:    zap.NewNop(),
-					client:    mockClient,
-					marshaler: marshaller,
+					cfg:        &cfg,
+					metrics:    newExporterMetrics([]byte{}, []byte{}, "", cfg.Namespace),
+					logger:     zap.NewNop(),
+					grpcClient: mockClient,
+					marshaler:  marshaller,
 				}
 			},
 			setupMocks: func(_ *mocks.MockIngestionServiceV2Client) {
@@ -132,11 +137,11 @@ func TestLogsDataPusher(t *testing.T) {
 				// Return an empty slice to simulate no logs to push
 				marshaller.On("MarshalRawLogs", mock.Anything, mock.Anything).Return([]*api.BatchCreateLogsRequest{}, nil)
 				return &chronicleExporter{
-					cfg:       &cfg,
-					metrics:   newExporterMetrics([]byte{}, []byte{}, "", cfg.Namespace),
-					logger:    zap.NewNop(),
-					client:    mockClient,
-					marshaler: marshaller,
+					cfg:        &cfg,
+					metrics:    newExporterMetrics([]byte{}, []byte{}, "", cfg.Namespace),
+					logger:     zap.NewNop(),
+					grpcClient: mockClient,
+					marshaler:  marshaller,
 				}
 			},
 			setupMocks: func(_ *mocks.MockIngestionServiceV2Client) {
@@ -149,7 +154,7 @@ func TestLogsDataPusher(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run(tc.desc, func(t *testing.T) {
 			exporter := tc.setupExporter()
-			tc.setupMocks(exporter.client.(*mocks.MockIngestionServiceV2Client))
+			tc.setupMocks(exporter.grpcClient.(*mocks.MockIngestionServiceV2Client))
 
 			// Create a dummy plog.Logs to pass to logsDataPusher
 			logs := mockLogs(mockLogRecord("Test body", map[string]any{"key1": "value1"}))
@@ -168,4 +173,52 @@ func TestLogsDataPusher(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestReallyPushToChronicle(t *testing.T) {
+	f := NewFactory()
+	cfg := f.CreateDefaultConfig().(*Config)
+	cfg.CustomerID = "b536658e-469e-44a5-b764-d5ab15b72ce0"
+	cfg.Protocol = protocolHTTPS
+	cfg.CredsFilePath = "/tmp/chronicle_creds.json"
+	cfg.Project = "telemetry-sandbox-340915"
+	cfg.Forwarder = "fbcb7d35-dd6f-4f20-aa2e-5ab2cceffa3c"
+	cfg.Location = "us"
+	cfg.Endpoint = "chronicle.googleapis.com"
+	cfg.LogType = "OFFICE_365"
+	settings := exportertest.NewNopSettings()
+
+	exp, err := f.CreateLogs(context.Background(), settings, cfg)
+	require.NoError(t, err)
+
+	require.NoError(t, exp.Start(context.Background(), componenttest.NewNopHost()))
+
+	logs := generateFakeLogs(1)
+
+	require.NoError(t, exp.ConsumeLogs(context.Background(), logs))
+
+	require.NoError(t, exp.Shutdown(context.Background()))
+}
+
+func generateFakeLogs(numLogs int) plog.Logs {
+	logs := plog.NewLogs()
+	// Configure resource attributes
+	res := logs.ResourceLogs().AppendEmpty()
+	res.Resource().Attributes().PutStr("service.name", "google-secops-log-generator")
+	res.Resource().Attributes().PutStr("log_type", "OFFICE_365")
+
+	// Add log entry
+	scopeLogs := res.ScopeLogs().AppendEmpty()
+	for i := 0; i < numLogs; i++ {
+		log := scopeLogs.LogRecords().AppendEmpty()
+		log.SetSeverityNumber(plog.SeverityNumberInfo)
+		log.Body().SetStr("User login detected.")
+		log.Attributes().PutStr("operation_name", "UserLogin")
+		log.Attributes().PutStr("user_email", "user@example.com")
+		log.Attributes().PutStr("status", "Success")
+		log.Attributes().PutStr("ip_address", "203.0.113.42")
+		log.SetTimestamp(pcommon.NewTimestampFromTime(time.Now()))
+	}
+
+	return logs
 }
